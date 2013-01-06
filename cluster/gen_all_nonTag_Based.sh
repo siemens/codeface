cca60afc0f3177aba1c05b1a17f223ae9b052c4c
Mitchell Joblin <joblin.m@gmail.com>
2013-01-06 15:48:40 +0100

    New scripts to generate graphs and data for tag and non-tag based analysis. These are nearly the same as the previous gen_all.sh script but now address the subtle differences between calling the python scripts to perform the two different analyses.
    Signed-off-by: Mitchell Joblin <joblin.m@gmail.com>

---
+++ cluster/gen_all_nonTag_Based.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+
+BASEDIR=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster
+CLUSTER=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/cluster.py
+CONV=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/conv.py
+PERSONS=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/persons.r
+REPORT=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/create_report.pl
+
+for i in "$@"; do
+    echo "Processing ${i}"
+    ${CLUSTER} /Users/Mitchell/git/linux-2.6/.git ${BASEDIR}/res/ ${i} --create_db --nonTag
+    ${PERSONS} ${BASEDIR}/res/${i} nonTag
+    (cd ${BASEDIR}/res/$i;
+	for file in `ls sg*.dot wt*.dot`; do 
+	    basefile=`basename $file .dot`; 
+	    echo "Processing $file"; 
+	    cat $file | ${CONV} | sfdp -Tpdf > ${basefile}.pdf; 
+	done)
+
+
+    if [[ ! (-d "${BASEDIR}/res/latex") ]]; then
+	echo mkdir ${BASEDIR}/res/latex
+    fi
+
+    ${REPORT} ${BASEDIR}/res/${i} "${i}..$((i+1))" > ${BASEDIR}/res/latex/report_${i}.tex;
+    (cd ${BASEDIR/res/latex} && pdflatex ${BASEDIR}/res/latex/report_${i}.tex)
+done
