9f41e6184389d745dd8a2a4fec1d4f51ae2d7033
Mitchell Joblin <joblin.m@gmail.com>
2013-01-09 14:21:55 +0100

    Change the storage locations for results to be slightly more organized.
    This change prevents overwriting the data when running the tag and
    non-tag analysis one after the other.
    
    Signed-off-by: Mitchell Joblin <joblin.m@gmail.com>

--- cluster/gen_all_Tag_Based.sh
+++ cluster/gen_all_Tag_Based.sh
@@ -8,9 +8,9 @@ REPORT=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/create_report.pl
 
 for i in "$@"; do
     echo "Processing ${i}"
-    ${CLUSTER} /Users/Mitchell/git/linux-2.6/.git ${BASEDIR}/res/ ${i} --create_db
-    ${PERSONS} ${BASEDIR}/res/${i} Tag
-    (cd ${BASEDIR}/res/$i;
+    #${CLUSTER} /Users/Mitchell/git/linux-2.6/.git ${BASEDIR}/res_Tag/ ${i} --create_db
+    #${PERSONS} ${BASEDIR}/res_Tag/${i} tag
+    (cd ${BASEDIR}/res_Tag/$i;
 	for file in `ls sg*.dot wt*.dot`; do 
 	    basefile=`basename $file .dot`; 
 	    echo "Processing $file"; 
@@ -18,10 +18,10 @@ for i in "$@"; do
 	done)
 
 
-    if [[ ! (-d "${BASEDIR}/res/latex") ]]; then
-	echo mkdir ${BASEDIR}/res/latex
+    if [[ ! (-d "${BASEDIR}/res_Tag/latex") ]]; then
+	   mkdir ${BASEDIR}/res_Tag/latex
     fi
 
-    ${REPORT} ${BASEDIR}/res/${i} "${i}..$((i+1))" > ${BASEDIR}/res/latex/report_${i}.tex;
-    (cd ${BASEDIR/res/latex} && pdflatex ${BASEDIR}/res/latex/report_${i}.tex)
+    ${REPORT} ${BASEDIR}/res_Tag/${i} "${i}..$((i+1))" > ${BASEDIR}/res_Tag/latex/report_${i}.tex;
+    (cd ${BASEDIR/res_Tag/latex} && pdflatex -output-directory=${BASEDIR}/res_Tag ${BASEDIR}/res_Tag/latex/report_${i}.tex)
 done
--- cluster/gen_all_nonTag_Based.sh
+++ cluster/gen_all_nonTag_Based.sh
@@ -4,13 +4,13 @@ BASEDIR=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster
 CLUSTER=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/cluster.py
 CONV=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/conv.py
 PERSONS=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/persons.r
-REPORT=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/create_report.pl
+REPORT=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster/create_report_nonTag.pl
 
 for i in "$@"; do
     echo "Processing ${i}"
-    #${CLUSTER} /Users/Mitchell/git/linux-2.6/.git ${BASEDIR}/res/ ${i} --nonTag
-    #${PERSONS} ${BASEDIR}/res/${i} nonTag
-    (cd ${BASEDIR}/res/$i;
+    ${CLUSTER} /Users/Mitchell/git/linux-2.6/.git ${BASEDIR}/res_NonTag/ ${i} --nonTag
+    ${PERSONS} ${BASEDIR}/res_NonTag/${i} nonTag
+    (cd ${BASEDIR}/res_NonTag/$i;
 	for file in `ls sg*.dot wt*.dot`; do 
 	    basefile=`basename $file .dot`; 
 	    echo "Processing $file"; 
@@ -18,10 +18,10 @@ for i in "$@"; do
 	done)
 
 
-    if [[ ! (-d "${BASEDIR}/res/latex") ]]; then
-	echo mkdir ${BASEDIR}/res/latex
+    if [[ ! (-d "${BASEDIR}/res_NonTag/latex") ]]; then
+        mkdir ${BASEDIR}/res_NonTag/latex
     fi
 
-    ${REPORT} ${BASEDIR}/res/${i} "${i}..$((i+1))" > ${BASEDIR}/res/latex/report_${i}.tex;
-    (cd ${BASEDIR/res/latex} && pdflatex ${BASEDIR}/res/latex/report_${i}.tex)
+    ${REPORT} ${BASEDIR}/res_NonTag/${i} "${i}..$((i+1))" > ${BASEDIR}/res_NonTag/latex/report_${i}.tex;
+    (cd ${BASEDIR/res_NonTag/latex} && pdflatex -output-directory=${BASEDIR}/res_NonTag ${BASEDIR}/res_NonTag/latex/report_${i}.tex)
 done
