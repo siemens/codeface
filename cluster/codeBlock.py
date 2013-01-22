4557e1cdf329f9aa662d35c041276e38536f0081
Mitchell Joblin <joblin.m@gmail.com>
2013-01-22 12:51:29 +0100

    added attribute to store dictionary of codeLine objects. This will help in later stages of processing codeBlock objects when examining relationship between committers and authors
    Signed-off-by: Mitchell Joblin <joblin.m@gmail.com>

--- cluster/codeBlock.py
+++ cluster/codeBlock.py
@@ -1,7 +1,25 @@
+import codeLine
+
 class codeBlock:
     
-    def __init__(self, start=None, end=None, id=None):
+    #===========================
+    # Constructors
+    #===========================
+    
+    def __init__(self, start=None, end=None, id=None, codeLines=None):
         
         self.start = start #start of the code block
         self.end   = end   #end of the code block
         self.id    = id    #reference to contributor 
+        self.codeLines = codeLines
+        
+    #===========================
+    # Accessors and Modifiers
+    #===========================
+    
+    def get_codeLines(self):
+        return self.codeLines
+    
+    
+    def add_codeLine(self, lineNum, cmtHash, authorId, committerId):
+        self.codeLines.append( codeLine.codeLine(lineNum, cmtHash, authorId, committerId) )
