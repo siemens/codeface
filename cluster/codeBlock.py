2e283c594dbfced64b31db191d8ee9dbce47c9b4
Mitchell Joblin <joblin.m@gmail.com>
2013-01-31 17:14:21 +0100

    addition of attributes to support different method for identifying codeblocks
    old method relied on using names rather than commit hash which allows us
    to better differentiate commit contributions in time
    
    Signed-off-by: Mitchell Joblin <joblin.m@gmail.com>

--- cluster/codeBlock.py
+++ cluster/codeBlock.py
@@ -1,18 +1,23 @@
 import codeLine
 
 class codeBlock:
+    '''
+    A code block is a contiguous set of lines of code from a single 
+    commit
+    '''
+    
     
     #===========================
     # Constructors
     #===========================
     
-    def __init__(self, start=None, end=None, id=None, codeLines=None):
+    def __init__(self, start=None, end=None, authorId=None, committerId=None, cmtHash=None):
         
-        self.start = start #start of the code block
-        self.end   = end   #end of the code block
-        self.id    = id    #reference to contributor 
-        self.codeLines = codeLines
-        
+        self.start       = start #start of the code block
+        self.end         = end   #end of the code block
+        self.authorId    = authorId 
+        self.committerId = committerId 
+        self.cmtHash     = cmtHash
     #===========================
     # Accessors and Modifiers
     #===========================
