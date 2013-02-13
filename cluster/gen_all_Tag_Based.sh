037c05f55ac6e25baeaaae9ab9dc580164196597
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-02-13 16:25:45 +0100

    More systematic storing of result files
    Make the output directories more systematic.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- cluster/gen_all_Tag_Based.sh
+++ cluster/gen_all_Tag_Based.sh
@@ -3,31 +3,40 @@
 #==================
 #   Configure
 #==================
-BASEDIR=/Users/Mitchell/Documents/workspace/prosoda_repo/cluster
-GITREPO=/Users/Mitchell/git/linux-2.6/.git
+BASEDIR=/home/wolfgang/projects/swi/prosoda/cluster
+GITDIR=/home/wolfgang/git-repos/
+PROJECT=linux
 #=================================================================
 
 CLUSTER=${BASEDIR}/cluster.py
 CONV=${BASEDIR}/conv.py
 PERSONS=${BASEDIR}/persons.r
 REPORT=${BASEDIR}/create_report.pl
+GITREPO=${GITDIR}/${PROJECT}/.git
+TAG_OPT="non_tag"
 
 for i in "$@"; do
-    echo "Processing ${i}"
-    ${CLUSTER} ${GITREPO} ${BASEDIR}/res_Tag/ ${i} --create_db
-    ${PERSONS} ${BASEDIR}/res_Tag/${i} tag
-    (cd ${BASEDIR}/res_Tag/$i;
+    VERSION=v2.6.$((i+1))
+    echo "Processing ${VERSION}"
+
+    ${CLUSTER} ${GITREPO} ${PROJECT} ${BASEDIR}/res/${PROJECT}/tag \
+	v2.6.${i} v2.6.$((i+1)) v2.6.$((i+1))-rc1 --create_db
+
+    ${PERSONS} ${BASEDIR}/res/${PROJECT}/tag/${VERSION} tag
+
+    (cd ${BASEDIR}/res/${PROJECT}/tag/${VERSION};
 	for file in `ls sg*.dot wt*.dot`; do 
 	    basefile=`basename $file .dot`; 
 	    echo "Processing $file"; 
 	    cat $file | ${CONV} | sfdp -Tpdf -Gcharset=latin1 > ${basefile}.pdf; 
 	done)
 
-
-    if [ ! -d "${BASEDIR}/res_Tag/latex" ]; then
-	   mkdir ${BASEDIR}/res_Tag/latex
+    if [ ! -d "${BASEDIR}/res/${PROJECT}/tag/latex" ]; then
+	   mkdir ${BASEDIR}/res/${PROJECT}/tag/latex
     fi
 
-    ${REPORT} ${BASEDIR}/res_Tag/${i} "${i}..$((i+1))" > ${BASEDIR}/res_Tag/latex/report_${i}.tex;
-    (cd ${BASEDIR}/res_Tag/latex && pdflatex -output-directory=${BASEDIR}/res_Tag ${BASEDIR}/res_Tag/latex/report_${i}.tex)
+    ${REPORT} ${BASEDIR}/res/${PROJECT}/tag/${VERSION} "${i}..$((i+1))" > ${BASEDIR}/res/${PROJECT}/tag/latex/report_${VERSION}.tex;
+    (cd ${BASEDIR}/res/${PROJECT}/tag/latex && \
+	pdflatex -output-directory=${BASEDIR}/res/${PROJECT}/tag/ \
+	${BASEDIR}/res/${PROJECT}/tag/latex/report_${VERSION}.tex)
 done
