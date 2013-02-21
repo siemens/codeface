cfe5bafca829586ba7be2aac81998efb1986dd0f
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-02-21 15:48:43 +0100

    Automate time series analysis
    Add a new R file to automate the analysis of a complete
    growth graph for a project. Currently, the overview graphic
    is produced, comprising the cumulative and raw series, the
    latter being smoothed with a small (1 day) and large (1 week)
    window.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

---
+++ analyse_ts.r
@@ -0,0 +1,210 @@
+#! /usr/bin/env Rscript
+
+# This file is part of prosoda.  prosoda is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+# All Rights Reserved.
+
+# Time series analysis based on the output of ts.py
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(zoo))
+suppressPackageStartupMessages(library(xts))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(plyr))
+suppressPackageStartupMessages(library(yaml))
+source("utils.r")
+source("config.r")
+
+# Omit time series elements that exceed the given range
+trim.series <- function(series, start, end) {
+  series <- series[which(index(series) < end),]
+  series <- series[which(index(series) > start),]
+  
+  return(series)
+}
+
+# Unidirectional version of the above function
+trim.series.start <- function(series, start) {
+  return(series[which(index(series) > start),])
+}
+
+get.series.boundaries <- function(conn) {
+  ## Read the first (comment) line of the time series connection,
+  ## and split it into start/end date timestamp, and possibly also
+  ## the release candidate start time stamp (the first entry is
+  ## the hash mark of the comment line)
+  elems = str_split(readLines(conn, n=1), "\t")[[1]][-1:0]
+
+  boundaries <- data.frame(date.start = tstamp_to_date(elems[1]),
+                           date.end = tstamp_to_date(elems[2]))
+
+  if (length(elems) == 3) {
+    boundaries$date.rc_start= tstamp_to_date(elems[3])
+  } else {
+    boundaries$date.rc_start = NA
+  }
+
+  return (boundaries)
+}
+
+boundaries.include.rc <- function(boundaries) {
+  return (!is.na(boundaries$rc_start))
+}
+
+# Given a list of time series file names, compute the total time series
+gen.full.ts <- function(ts.file.list) {
+  full.series <- vector("list", length(ts.file.list))
+  releases <- vector("list", length(ts.file.list))
+  
+  for (i in 1:length(ts.file.list)) {
+    conn <- file(ts.file.list[[i]], "r")
+    boundaries <- get.series.boundaries(conn)
+    
+    full.series[[i]] <- read.zoo(conn, FUN=tstamp_to_date)[,1]    
+    full.series[[i]] <- trim.series(full.series[[i]], boundaries$date.start,
+                                    boundaries$date.end)
+
+    releases[[i]] <- data.frame(date.release=boundaries$date.end,
+                                date.rc_start=boundaries$date.rc_start)
+    close(conn)
+  }
+
+  full.series <- do.call(c, full.series)
+  releases <- data.frame(do.call(rbind, releases))
+  
+  return (list(series=full.series, releases=releases))
+}
+
+## Convert a time series into a data frame
+gen.df.from.ts <- function(ts, type) {
+  df <- data.frame(time=index(ts), value=coredata(ts),
+                   value.scaled=scale.data(coredata(ts)), type=type)
+
+  return(df)
+}
+
+gen.file.list <- function(basedir, revisions) {
+  ts.file.list <- vector("list", length(revisions)-1)
+
+  for (i in 2:length(revisions)) {
+    ts.file.list[[i-1]] <- paste(basedir, "/ts/raw_", revisions[[i-1]], "-",
+                               revisions[[i]], ".dat", sep="")
+  }
+
+  return(ts.file.list)
+}
+
+# NOTE: width is the width of the rolling window, so series.monthly
+# does _not_ mean that there is one data point per month, but one
+# month's worth of data points go into the calculation of one smoothed
+# data point. Using the robust median instead of mean considerably
+# reduces the amount of outliers
+gen.series.df <- function(series) {
+  duration <- end(series) - start(series)
+
+  ## We compute the window lengths based on natural time units
+  ## to avoid dependencies on the lifetime of the project, or on the
+  ## project's relative activity
+  ## Assume that a month has roughly 30 days.
+  width.monthly <- length(series)/as.numeric(duration/7)
+  width.daily <- length(series)/as.numeric(duration)
+  series.daily <- rollapply(series, width=width.daily, median, align="center")
+  series.monthly <- rollapply(series, width=width.monthly, median, align="center")
+  series.cumulative <- cumsum(series)
+
+  ## NOTE: R can only merge two data frames at once
+  series.merged <- merge(gen.df.from.ts(series.daily,
+                                        type="Averaged (small window)"),
+                         gen.df.from.ts(series.monthly,
+                                        type="Averaged (large window)"), all=T)
+  series.merged <- merge(series.merged,
+                         gen.df.from.ts(series.cumulative,
+                                        type="Cumulative"), all=T)
+
+  return(series.merged)
+}
+
+do.ts.analysis <- function(basedir, config.file) {
+  conf <- load.config(config.file)
+  basedir <- paste(basedir, conf$project, conf$tagging, sep="/")
+  graphdir <- paste(basedir, "graphs", sep="/")
+  dir.create(graphdir, showWarnings=FALSE, recursive=TRUE)
+  
+  ts.file.list <- gen.file.list(basedir, conf$revisions)
+  
+  ## Dispatch the calculations and create result data frames
+  full.ts <- gen.full.ts(ts.file.list)
+  series.merged <- gen.series.df(full.ts$series)
+  
+  ## Prepare y ranges for the different graph types
+  ## Compute min/max value per type
+  ranges <- ddply(series.merged, .(type), summarise,
+                  ymin=min(value), ymax=max(value))
+  
+  num.types <- length(ranges)
+  res <- vector("list", num.types)
+  for (i in 1:num.types) {
+    res[[i]] <- cbind(full.ts$releases, ranges[i,])
+  }
+  full.ts$releases <- do.call(rbind, res)
+
+  ## Release cycles without release candidates must be removed
+  ## (otherwise, we run into plotting problems)
+  dat.rc <- na.omit(full.ts$releases)
+  dat.rc$date.rc_start <- tstamp_to_date(dat.rc$date.rc_start)
+
+  ## Visualisation
+  ## TODO: log and sqrt transform are reasonable for the averaged, but not
+  ## for the cumulative series
+  g <- ggplot(series.merged, aes(x=time, y=value)) + geom_line() +
+    facet_grid(type~., scale="free_y") +
+    geom_vline(aes(xintercept=as.numeric(date.release), colour="red"),
+               data=full.ts$releases) +
+    scale_fill_manual(values = alpha(c("blue", "red"), .1)) +
+    xlab("Time") + ylab("Amount of changes") +
+    ggtitle(paste("Code changes for project '", conf$description, "'", sep=""))
+
+  if (dim(dat.rc)[1] > 0) {
+    ## Only plot release candidate regions if there are any, actually
+    g <- g + geom_rect(aes(NULL, NULL, xmin=date.rc_start,
+                           xmax=date.release, ymin=ymin, ymax=ymax, fill="blue"),
+                       data=dat.rc)
+
+  }
+  
+  ggsave(paste(graphdir, "ts.pdf", sep="/"), g, width=16, height=7)
+  ## All-in-one graph with scaled axes (ugly)
+#  g <- ggplot(series.merged, aes(x=time, y=value.scaled, colour=type)) +
+#    geom_line() + geom_vline(aes(xintercept=as.numeric(rel.date)),
+#                             data=full.ts$releases)
+#  print(g)
+}
+
+######################### Dispatcher ###################################
+## TODO: Actually, write a dispatcher
+parser <- OptionParser(usage = "%prog basedir config")
+arguments <- parse_args(parser, positional_arguments = TRUE)
+
+if (length(arguments$args) != 2) {
+  cat("Please specify base directory and configuration file\n")
+  print_help(parser)
+  stop()
+} else {
+  basedir <- arguments$args[1]
+  config.file <- arguments$args[2]
+}
+
+do.ts.analysis(basedir, config.file)
