7b84b2cbd32d49cc0cdb0504a209261ccd27063c
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-02-21 15:46:54 +0100

    Define a yaml configuration file format to describe projects
    This eliminates the need to pass all parameters via command
    line options, which is cumbersome. Simple parsers are provided
    for R and python.
    
    The configuration file specifies the local name under which the
    analysis is stored in the file system (project), the identifier
    of the git repository (repo), a human readable textual identifier
    for the project (description -- use only a few words), and
    the revision path that should be traversed. Release candidate
    revisions that map to the revisions are specified in rcs. This
    element may be omitted if the project does not use release candidates.
    Versions for which no rc exists are specified with an empty string ("").
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

---
+++ config.r
@@ -0,0 +1,37 @@
+# This file is part of prosoda.  prosoda is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+# All Rights Reserved.
+
+## Load configuration and apply some (yet uncomprehensive) sanity checks
+require(yaml)
+
+load.config <- function(file) {
+  conf <- yaml.load_file(file)
+
+  if (is.null(conf$project) || is.null(conf$repo)) {
+    stop("Malformed configuration: Specify project and repository!\n")
+  }
+  
+  if (conf$tagging != "tag" && conf$tagging != "notag") {
+    stop("Malformed configuration: Neither 'tag' nor 'notag' specified as tagging mode!")
+  }
+
+  if(length(conf$revisions) < 2) {
+    stop("Malformed configuration: Revision list must include at least 2 commits!")
+  }
+
+  return(conf)
+}
+
