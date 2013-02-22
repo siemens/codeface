cc14ca213461a1a39516b387dfee80747d90403b
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-02-22 16:52:28 +0100

    Add copyright header to the report generation script
    As usual, it's licensed by GPLv2.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- cluster/create_report.pl
+++ cluster/create_report.pl
@@ -1,5 +1,22 @@
 #! /usr/bin/env perl
 
+# This file is part of prosoda.  prosoda is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2010, 2011 by Wolfgang Mauerer <wm@linux-kernel.net>
+# Copyright 2012, 2013, Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+# All Rights Reserved.
+
 use strict;
 
 # Create \includegraphics  or \input (in minipages) commands for a number of images (in $files_ptr)
