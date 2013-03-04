6aae72c6adf43a20e898e628eafb0b00b58a652c
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-03-04 09:55:07 +0100

    Provide a list of all cluster data files
    This is very similar to the other lists of files for
    time series. We do keep separate functions to ease the transition
    to a proper data base, though.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- analyse_ts.r
+++ analyse_ts.r
@@ -110,6 +110,9 @@ gen.rev.list <- function(revisions) {
  return (rev.list)
 }
 
+## NOTE: Although the gen.xyz.file.list functions are fairly similar,
+## they are kept separately because they will be replaced by slightly
+## different queries to a general data base later
 gen.ts.file.list <- function(resdir, revisions) {
   ts.file.list <- vector("list", length(revisions)-1)
 
@@ -122,14 +125,26 @@ gen.ts.file.list <- function(resdir, revisions) {
 }
 
 gen.commit.file.list <- function(resdir, revisions) {
-  ts.file.list <- vector("list", length(revisions)-1)
+  file.list <- vector("list", length(revisions)-1)
 
   revs <- gen.rev.list(revisions)
   for (i in 1:length(revs)) {
-    ts.file.list[[i]] <- paste(resdir, "/", revs[[i]], "/commits.txt", sep="")
+    file.list[[i]] <- paste(resdir, "/", revs[[i]], "/commits.txt", sep="")
   }
 
-  return(ts.file.list)
+  return(file.list)
+}
+
+gen.cluster.file.list <- function(resdir, revisions, type) {
+  file.list <- vector("list", length(revisions)-1)
+
+  revs <- gen.rev.list(revisions)
+  for (i in 1:length(revs)) {
+    file.list[[i]] <- paste(resdir, "/", revs[[i]], "/", type,
+                            "_cluster_stats.txt", sep="")
+  }
+
+  return(file.list)
 }
 
 # NOTE: width is the width of the rolling window, so series.monthly
