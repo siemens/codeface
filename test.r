7d34fe1f18986e4de1d9880569cfb6502b757076
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-03-19 11:06:07 +0100

    Un-bitrot the experimental testing code
    Get the code aligned with recent changes in the
    general infrastructure.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- test.r
+++ test.r
@@ -1,101 +1,63 @@
-# TODO: Create a class that carries all intermediate results so
-# that they can easily be passed to project-specific post-processing
-# functions
-# TODO: We could use the same mining techniques on the repository:
-# Check which authors touched whose code (to get author-author relationships),
-# and use the commit messages to get textual information
-# Combine both pieces as in this analysis
-# TODO: Analyse fixed time batches of mailing lists (for instance, over one
-# month) to generate time series from the results computed for each
-# batch -- this will give some insights how a project developed over time
-# Question is: Should we split by time, or should we split by a fixed
-# number of messages? (e.g, using tm::makeChunk)
-# TODO: When plots with stemmed words are displayed, we should use
-# tm::stemCompletion to (try to) provide complete words in the graph
-# TODO: Use the MPI facilities provided by tm: tm_startCluster(), tm_endCluster()
-doCompute <- TRUE # If set to F, load computed results from cached files
-library(tm)
-library(tm.plugin.mail)
-library(sna)
-#library(wordnet)
-library(ggplot2)
-library(igraph)
-library(lsa)
-library(Rgraphviz) # See www.bioconductor.org/packages/2.9/bioc/html/Rgraphviz.html
-library(lubridate)
-library(multicore)
+## TODO: Create a class that carries all intermediate results so
+## that they can easily be passed to project-specific post-processing
+## functions
+## TODO: We could use the same mining techniques on the repository:
+## Check which authors touched whose code (to get author-author relationships),
+## and use the commit messages to get textual information
+## Combine both pieces as in this analysis
+## TODO: Analyse fixed time batches of mailing lists (for instance, over one
+## month) to generate time series from the results computed for each
+## batch -- this will give some insights how a project developed over time
+## Question is: Should we split by time, or should we split by a fixed
+## number of messages? (e.g, using tm::makeChunk)
+## TODO: When plots with stemmed words are displayed, we should use
+## tm::stemCompletion to (try to) provide complete words in the graph
+doCompute <- FALSE # TRUE # If set to FALSE, load computed results from cached files
+analysis.base <- "/home/wolfgang/projects/swi/"
+data.path.base <- "/home/wolfgang/projects/swi/ml_results"
 
-source("local.r")
-source("analysis.r")
-source("project.spec.r")
-source("keyword.list.r")
-# Load the contents of snatm manually -- things are still quite in flux
-snatm.path <- "/home/wolfgang/projects/zk/QuantArch/src/snatm/pkg/R"
-sourceDir(snatm.path)
-# Wordnet database can be downloaded from
-# http://wordnet.princeton.edu/wordnet/download/current-version/
-#setDict("/home/wolfgang/projects/zk/QuantArch/wordnet/dict")
+project <- "git"
+data.prefix="4weekly"
+i <- 2
 
-repo.path <- "/home/wolfgang/projects/zk/QuantArch/nntp/repos/"
-#ml <- "gmane.os.freebsd.architechture"
-#ml <- "gmane.linux.kernel"
-#ml <- "gmane.comp.version-control.git"
-ml <- "gmane.comp.lang.d.general"
-#ml <- "gmane.comp.compilers.clang.devel"
-#ml <- "gmane.comp.mozilla.general"
-#ml <- "gmane.os.openbsd.tech"
-data.path <- "/media/disk/QA/"
+## When the analysis uses the standard directory layout, these
+## definitions are not supposed to be changed
+repo.path <- paste(analysis.base, "nntp/repos/", sep="/")
+snatm.path <- paste(analysis.base, "src/snatm/pkg/R", sep="")
+source("includes.r")
 
-## TODO: Filter out spam. There's an incredible amount in some gmane archives
-## TODO: (this should also include filtering out non-english messages)
-## The easiest thing would be to let SpamAssassin run over the mbox
-## file, and then delete all messages marked as SPAM.
+data.path <- file.path(data.path.base, project, "ml", paste(data.prefix, i, sep="."))
 
-# TODO: The analysis (sometimes...) takes an order of magnitude longer with
-# UTF-8 vs. latin1?! Could be the stemming, but check with Rprof.
-date()
-options(error=recover)
-dispatch.all(ml, repo.path, data.path, doCompute=TRUE)
-date()
+## Read in res (as saved by dispatch.steps)
+load(file=file.path(data.path, "vis.data"))
 
-quit()
-## TODO: Sometimes, the analysis runs for aaaaaaages. In the next run
-## (after a restart of R, so in a fresh environment), everything is fine?! -> Why?!
-## NOTE: It occurs especially overnight that the computation seems to freeze
-## at some point (when run from ESS, batch mode needs to be checked). Seems like
-## something is waiting for something, perhaps the RNG generator.
-## TODO: This would be a good candidate for cluster-level parallelism
+## Read in forest.corp{,.sub} (as saved by dispatch.sub.sequences)
+load(file=file.path(data.path, "forest.corp"))
+forest.corp <- forest.corp.sub
+forest <- forest.corp$forest
+corp <- forest.corp$corp
+rm(forest.corp.sub)
 
-for (ml in c("gmane.comp.compilers.clang.devel",
-             "gmane.comp.lang.d.general",
-             "gmane.comp.version-control.git", "gmane.linux.kernel",
-             "gmane.os.freebsd.architechture", "gmane.os.openbsd.tech")) {
-  cat ("Analysing ", ml, ". Start: ", date(), "\n", sep="")
-  t <- Sys.time()
-  dispatch.all(ml, repo.path, data.path, doCompute=TRUE)
-  cat ("Finished: ", date(), "\n", sep="")
-  cat ("Duration: ", Sys.time() - t, "\n")
-}
 
-################## TODO: Experimental stuff #####################
-##################### Classical text mining analysis ##################
-findAssocs(doc.matrices$tdm, "submodul", 0.75)
-findAssocs(doc.matrices$tdm, "llvm", 0.6)
+# ################# TODO: Experimental stuff #####################
+# #################### Classical text mining analysis ##################
+findAssocs(res$doc.matrices$tdm, "submodul", 0.75)
+findAssocs(res$doc.matrices$tdm, "llvm", 0.6)
 # TODO: Explore further standard methods described in the tm manual
 
-################### Classification ##################
-# TODO: Classify the messages into interesting categories. This is
-# very important -- for instance, it could be used to automatically
-# filter out security relevant topics (and then track their progress
-# to the repository)
-# The tm package has some standard means for doing this, check if the
-# methods deliver satisfactory results
+## ################# Classification ##################
+## TODO: Classify the messages into interesting categories. This is
+## very important -- for instance, it could be used to automatically
+## filter out security relevant topics (and then track their progress
+## into the repository)
+## The tm package has some standard means for doing this, check if the
+## methods deliver satisfactory results
 
-########## Exploratory/descriptive statistics for the data ###############
-# Compute some exploratory statistics on the data
-# TODO: See how far this is already handled by generic methods
-# provided by the tm package
-forest <- forest.corp$forest
+## ######## Exploratory/descriptive statistics for the data ###############
+## Compute some exploratory statistics on the data
+## NOTE: Some of these are also approximately handled by generic tm methods,
+## but the implementation here is more accurate
+
 authors.per.thread <- function(i) {
   length(unique(forest[forest[,"threadID"]==i, "author"]))
 }
@@ -104,11 +66,26 @@ messages.per.thread <- function(i) {
   length(forest[forest[,"threadID"]==i, "subject"])
 }
 
+## Determine authors and messages _per thread_
 num.authors <- sapply(unique(forest[,"threadID"]), authors.per.thread)
 num.messages <- sapply(unique(forest[,"threadID"]), messages.per.thread)
+thread.info <- data.frame(authors=num.authors, messages=num.messages,
+                          tid=attr(num.messages, "names"))
 
-# Suboptimal (but we need a direct visualisation of the densities, not just
-# the ecdf)
+## Are there some focused discussions, or do changes 
+## TODO: Add a 45 degree slope line or a linear regression
+ggplot(thread.info, aes(x=authors, y=messages)) + geom_point()
+
+thread.info.molten <- melt(thread.info)
+ggplot(thread.info.molten, aes(x=variable, y=value)) + geom_boxplot() +
+  scale_y_log10()
+
+## Generate time series (authors active per time, messages per time,
+## durtation of discussions, TODO: what else)
+
+
+## Suboptimal (but we need a direct visualisation of the densities, not just
+## the ecdf)
 ggplot(data.frame(x=num.messages), aes(x=x)) + 
   geom_density(aes(y=..density..)) + scale_y_log10()
 
@@ -129,63 +106,88 @@ ggplot(data=dat, aes(x=x, y=y)) + geom_step() +
   xlab("Number of authors/messages per thread") +
   ylab("ecdf")
 
-# How does the number of authors scale with the number of messages per thread?
+## How does the number of authors scale with the number of messages per thread?
+## TODO: Also not really optimal. The ecdfs look way to similar for fairly different
+## mailing lists.
 ggplot(data.frame(x=num.messages, y=num.authors), aes(x=x, y=y)) + geom_point() +
   xlab("Number of messages") + ylab("Number of authors") + geom_smooth()
 
-# NOTE/TODO: It should be possible to detect topics/threads of large
-# relevance/interest using a weighted selection by number of authors and
-# number of messages.
+## NOTE/TODO: It should be possible to detect topics/threads of large
+## relevance/interest using a weighted selection by number of authors and
+## number of messages.
 get.subject <- function(i) {
   as.character(forest[forest[,"threadID"]==i, "subject"][1])
 }
 get.authors <- function(threadID) {
   unique(forest[forest[,"threadID"]==threadID, "author"])
 }
-# Select the 10 largest threads (measured by the number of messages in the
-# threads)
-largest.threads <- as.integer(names(sort(num.messages, decreasing=T)[1:10]))
-# ... and determine the subjects that started the threads
+## Select the largest threads (measured by the number of messages in the
+## threads)
+largest.threads <- as.integer(names(sort(num.messages, decreasing=T)[1:20]))
+## ... and determine the subjects that started the threads
 sapply(largest.threads, get.subject)
 
-# Same thing in blue, this time with the number of contributing authors
-# as size measure
-largest.threads <- as.integer(names(sort(num.authors, decreasing=T)[1:10]))
-sapply(largest.threads, get.subject)
-get.authors(largest.threads[2])
+dat.msg <- data.frame(tid=1:length(largest.threads),
+                      num.messages=sapply(largest.threads, function(x) length(get.subject(x))))
 
-# Determine all authors who contributed to the largest threads
-# (interestingly, there does not seem to be too much overlap for the linux
-# kernel: 162 authors with repetition, 128 unique authors.
-# Linux: 0.79, Freebsd: 0.69 (-> less overlap))
+ggplot(dat.msg[which(dat.msg$num.authors > 3),], aes(x=num.messages)) + geom_density() +
+  xlab("Messages per thread") + ylab("Density")
+
+## Same thing in blue, this time with the number of contributing authors
+## as size measure
+## TODO: Automatically select the number of threads under consideration
+largest.threads <- as.integer(names(sort(num.authors, decreasing=T)[1:250]))
+sapply(largest.threads, get.subject)
+
+dat.msg <- data.frame(tid=1:length(num.authors),
+                      num.authors=sort(num.authors, decreasing=T))
+ggplot(dat.msg[1:15,], aes(x=tid, y=num.authors)) + geom_bar(stat="identity") +
+       xlab("Thread (arbitrary ID)") + ylab("Authors in thread")
+## TODO: Most threads do only have one message. This seems curious to me. Check.
+
+## Looks like an exponential decay would describe this very well, at least for LKML
+ggplot(dat.msg[which(dat.msg$num.authors > 1),], aes(x=num.authors)) +
+       geom_histogram(binwidth=1) + scale_y_log10()
+  xlab("Authors per thread") + ylab("Density")
+
+## Determine all authors who contributed to the largest threads
+## (interestingly, there does not seem to be too much overlap for the linux
+## kernel: 162 authors with repetition, 128 unique authors.
+## Linux: 0.79, Freebsd: 0.69 (-> less overlap))
 authlist <- unlist(sapply(largest.threads, get.authors))
 length(unique(authlist))/length(authlist)
 unique(
        unlist(sapply(largest.threads, get.authors))
        )
 
-# Find the most active authors (i.e., loudmouths ;)) in the complete archive
-# (taken from the tm docs)
+## Find the most active authors (i.e., loudmouths ;)) in the complete archive
+## (taken from the tm docs)
 authors <- lapply(corp, Author)
 authors <- sapply(authors, paste, collapse = " ")
-sort(table(authors), decreasing = TRUE)[1:10]
+sort(table(authors), decreasing = TRUE)[1:50]
 
-# Same in blue for the headings (TODO: Check if this delivers the same
-# results as the thread analysis)
-# NOTE: The initiating message is not counted. Messages with subjects
-# along the line "Re: Re: subject", "AW: Re: AW: subject" will also
-# not be counted to a thread. The results are therefore only approximative
+## TODO: Export as table and plot distribution
+
+## TODO: Repeat this calculation for the different time periods, and then
+## compute the intersections of the author names with a sliding scale to see
+## how stable the number of central developers is.
+
+## Same in blue for the headings (TODO: Check if this delivers the same
+## results as the thread analysis)
+## NOTE: The initiating message is not counted. Messages with subjects
+## along the line "Re: Re: subject", "AW: Re: AW: subject" will also
+## not be counted to a thread. The results are therefore only approximative
 headings <- lapply(corp, Heading)
 headings <- sapply(headings, paste, collapse = " ")
 bigTopicsTable <- sort(table(headings), decreasing = TRUE)[1:10]
 names(bigTopicsTable)
-# TODO: This delivers a different result than our two custom methods.
-# Check how to use tm to determine importance of a thread.
+## TODO: This delivers a different result than our two custom methods.
+## Check how to use tm to determine importance of a thread.
 
 
-######### Descriptive statistics ###########
+## ####### Descriptive statistics ###########
 #- Lengths of threads (# of messages, #of days, #of authors, everything time resolved)
-#- mails per day, time-resolved
+##- mails per day, time-resolved
                        
                       
 #################### Keyword extraction ######################
@@ -193,18 +195,18 @@ library(RKEA)
 tmpdir <- tempfile()
 dir.create(tmpdir)
 model <- file.path(tmpdir, "testModel")
-# TODO: How the heck is the indexing supposed to work if there is
-# no pre-specified model? Should be possible as the the KEA documentation,
-# but the question is how this works with R
+## TODO: How the heck is the indexing supposed to work if there is
+## no pre-specified model? Should be possible as the the KEA documentation,
+## but the question is how this works with R
 createModel(corp, c(), model)
 
-# TODO: package wordcloud could come handy when we need to visualise our results
+## TODO: package wordcloud could come handy when we need to visualise our results
 
 
-############## Experimental section ###########
-# NOTE: For VCorpus objects, object[n] will trigger the print method (which just states
-# that the object is a corpus with a single document), whereas the document
-# itself can be retrieved via object[[n]]
+## ############ Experimental section ###########
+## NOTE: For VCorpus objects, evaluating object[n] will trigger the print
+## method (which just states that the object is a corpus with a single
+## document), whereas the document itself can be retrieved via object[[n]]
 
 library(openNLP)
 doc <- corp[[10160]]
@@ -250,6 +252,6 @@ num.weeks <- new_interval(gen.aligned.range(dates, "week")[[1]],
 max(dates.cleaned)-min(dates.cleaned)
 range(dates[which(dates >= m.start & dates < m.end)])
 
-# TODO: Analyse in two sequence modes: Weekly and four-weekly (or monthly?).
-# Align at the start of a week (aligning at the start of a month would likely
-# not bring any added benefits)
+## TODO: Analyse in two sequence modes: Weekly and four-weekly (or monthly?).
+## Align at the start of a week (aligning at the start of a month would likely
+## not bring any added benefits)
