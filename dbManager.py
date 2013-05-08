4ffdc930eebd71757777d34a083699ec49c2f030
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-05-08 09:18:39 +0200

    dbManager: Add error handlers
    Place the database operations into try..except blocks, propagate
    information about failure causes to the user, and provide wrappers
    for the augmented operations. Finally, convert the (little) existing
    code.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- dbManager.py
+++ dbManager.py
@@ -18,36 +18,65 @@
 # Thin sql database wrapper
 
 import MySQLdb as mdb
+import sys
 
 class dbManager:
     """This class provides an interface to the prosoda sql database."""
 
     def __init__(self, host, user, passwd, db):
-        self.con = mdb.Connection(host=host, user=user,
-                                  passwd=passwd, db=db)
+        try:
+            self.con = None
+            self.con = mdb.Connection(host=host, user=user,
+                                      passwd=passwd, db=db)
+        except mdb.Error, e:
+            print "Could not initialise database connection: %s (%d)" % \
+                    (e.args[1], e.args[0])
+            sys.exit(-1)
         self.cur = self.con.cursor()
 
     def __del__(self):
-        if self.con:
+        if self.con != None:
             self.con.close()
 
+    def doExec(self, stmt, args=None):
+        try:
+            return(self.cur.execute(stmt, args))
+        except mdb.Error, e:
+            print "Encountered mysql error %d during statement %s: %s" % \
+                    (stmt, e.args[0], e.args[1])
+            sys.exit(-1)
+
+    def doFetchAll(self):
+        try:
+            return(self.cur.fetchall())
+        except mdb.Error, e:
+            print "Encountered mysql error %d during fetchall: %s" % \
+                    (e.args[0], e.args[1])
+            sys.exit(-1)
+
+    def doCommit(self):
+        try: 
+            return(self.con.commit())
+        except mdb.Error, e:
+            print "Encountered mysql error %d during commit: %s" % \
+                (e.args[0], e.args[1])
+            sys.exit(-1)
 
     # NOTE: We don't provide any synchronisation since by assumption,
     # a single project is never analysed from two threads.
     # TODO: This does not consider the case that one project can
     # be analysed with different methods
     def getProjectID(self, name, analysisMethod):
-        self.cur.execute("SELECT id FROM project WHERE name=%s", name)
+        self.doExec("SELECT id FROM project WHERE name=%s", name)
 
         if self.cur.rowcount < 1:
             # Project is not contained in the database
-            print("Registering project")
-            self.cur.execute("INSERT INTO project (name, analysisMethod) " +
-                             "VALUES (%s, %s);", (name, analysisMethod))
-            res = self.con.commit()
-            self.cur.execute("SELECT id FROM project WHERE name=%s;", name)
+            self.doExec("INSERT INTO project (name, analysisMethod) " +
+                        "VALUES (%s, %s);", (name, analysisMethod))
+            self.doCommit()
+            self.doExec("SELECT id FROM project WHERE name=%s;", name)
 
-        res = self.cur.fetchall()[0]
+        res = self.doFetchAll()[0]
         return(res[0])
 
 ##### Test cases #####
