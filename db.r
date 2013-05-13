473897231665a8950890731a07b2d2f2ea5c3a58
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-05-13 08:35:03 +0200

    Add database helper routines file for R
    Will be similar to dbManager.py in the python world.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

---
+++ db.r
@@ -0,0 +1,32 @@
+## This file is part of prosoda.  prosoda is free software: you can
+## redistribute it and/or modify it under the terms of the GNU General Public
+## License as published by the Free Software Foundation, version 2.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+##
+## Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+## All Rights Reserved.
+
+## Generic database helper functions. More specific queries should
+## be directly included into the code
+
+## TODO: Add S3 database helper class similar to dbManager.py
+
+suppressPackageStartupMessages(library(stringr))
+
+sq <- function(string) {
+  return(str_c("'", string, "'"))
+}
+
+get.project.id <- function(con, name) {
+  res <- dbGetQuery(con, str_c("SELECT id FROM project WHERE name=", sq(name)))
+  
+  return(res$id)
+}
