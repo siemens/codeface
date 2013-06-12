dd543d9190160a8f5263caec123ae5a7ba83e82e
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-06-13 00:10:00 +0200

    Convenience view to replace ids.txt
    While table author_commits_stats contains all information to replace
    the file ids.txt, but the view introduced in this commit makes
    the information easier to access without having to do multiple
    queries from the analysis code.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- create_views.sql
+++ create_views.sql
@@ -12,3 +12,10 @@ FROM
 	LEFT JOIN release_timeline rt_rs ON rr.releaseRCStartId = rt_rs.id
 	JOIN project p ON rr.projectId = p.id
 order by rr.id asc
+
+CREATE VIEW `quantarch`.`author_commit_stats_view` AS 
+SELECT person.name as Name, author_commit_stats.author as ID, author_commit_stats.releaseRangeId, author_commit_stats.added, author_commit_stats.deleted, author_commit_stats.total, author_commit_stats.numcommits
+FROM author_commit_stats,person
+WHERE author IN (select distinct(author) FROM author_commit_stats) 
+      AND person.id=author_commit_stats.author
+GROUP BY author, releaseRangeId;
