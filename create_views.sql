9f7ac501f9f9b5a5827e9ba8f11d6f462bda62de
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-06-19 10:18:49 +0200

    Syntax fix for view creation script
    Add a missing semicolon.
    
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- create_views.sql
+++ create_views.sql
@@ -1,3 +1,5 @@
+USE `quantarch`;
+
 CREATE VIEW `quantarch`.`revisions` AS
 SELECT 
 	p.id as projectId,
@@ -11,7 +13,7 @@ FROM
 	JOIN release_timeline rt_e ON rr.releaseEndId = rt_e.id
 	LEFT JOIN release_timeline rt_rs ON rr.releaseRCStartId = rt_rs.id
 	JOIN project p ON rr.projectId = p.id
-order by rr.id asc
+order by rr.id asc;
 
 CREATE VIEW `quantarch`.`author_commit_stats_view` AS 
 SELECT person.name as Name, author_commit_stats.author as ID, author_commit_stats.releaseRangeId, author_commit_stats.added, author_commit_stats.deleted, author_commit_stats.total, author_commit_stats.numcommits
