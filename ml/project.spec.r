d4a3564c00a4ef9cdcc6d51deb05b8cb85ac7991
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-07-03 22:12:54 +0200

    ML preprocessing: Properly handle Unicode strings
    Instructing the grep() call to use byte-wise matching
    does not change the results, but silences a warning
    with strings in a broken encoding.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- ml/project.spec.r
+++ ml/project.spec.r
@@ -3,7 +3,7 @@
 linux.kernel.preprocess <- function(corp) {
   # Remove anything sent by a tip bot
   authors <- sapply(corp, Author)
-  tip.bot <- grep("tip-bot for ", authors, fixed=T)
+  tip.bot <- grep("tip-bot for ", authors, fixed=T, useBytes=T)
   if (length(tip.bot) > 0)
       corp <- corp[-tip.bot]
   
