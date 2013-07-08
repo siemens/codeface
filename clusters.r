a18a706c4fbde5dcabe04d70c5491733a9ebade9
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-07-08 23:14:25 +0200

    Move annotate.cluster to clusters.r
    The function is helpful in multiple contexts, so it should
    be visible outside the web interface cluster visualisation
    code.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- clusters.r
+++ clusters.r
@@ -14,7 +14,7 @@
 ## Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
 ## All Rights Reserved.
 
-## Definitions for clustering
+## Definitions and utility function for working with clusters
 
 cluster.methods <- c("Spin Glass Community", "Random Walk Community")
 pagerank.types <- c(0, 1) ## 0 mean regular, 1 means transposed adjacency matrix
@@ -25,4 +25,42 @@ cluster.method.valid <- function(cluster.method) {
 
 pagerank.type.valid <- function(pr.type) {
   return(any(pr.method %in% pr.type))
+}
+
+annotate.cluster <- function(g) {
+  V(g)$size <- sqrt(V(g)$rankValue*5000)
+  E(g)$width <- sqrt(E(g)$weight)
+
+  ## We store the global properties as attributes of the graph
+  ## to eliminate the need for a second data structure
+
+  ## Reciprocity computes the amount of reciprocal connections. The
+  ## larger the score, the better is bidirectional connection in a graph
+  ## (low score ones tend to be central developer communities with numerous
+  ## unconnected contributors)
+  g$rec <- round(reciprocity(g, ignore.loops=F), digits=3)
+
+  ## TODO: Document the purpose of graph strength
+  g$strength <- mean(graph.strength(g, mode="all"))
+  g$strength <- round(g$strength/vcount(g), digits=3)
+
+  ## Select the most important developers (as per page rank)
+  prank.sorted <- sort(V(g)$rankValue, index.return=T, decreasing=T)
+
+  ## We compute the degree for the three most important developers
+  ## (for large graphs) or of the most important developer for
+  ## small graphs.
+  ## TODO: The threshold is arbitrary
+  if (vcount(g) > 10) {
+    vertex.idx <- prank.sorted$ix[1:3]
+  } else {
+    vertex.idx <- prank.sorted$ix
+  }
+
+  g$deg.graph <- round(mean(degree(g, vertex.idx, normalize=T)), digits=3)
+  g$size <- vcount(g)
+
+  ## TODO: IN the analysis/the clustering phase, also consider the
+  ## page rank distribution and mean page rank.
+  return(g)
 }
--- dynamic_graphs/vis.clusters.r
+++ dynamic_graphs/vis.clusters.r
@@ -25,6 +25,7 @@ source("config.r")
 source("db.r")
 source("utils.r")
 source("query.r")
+source("clusters.r")
 
 ## Global variables
 conf <- load.global.config("prosoda.conf")
@@ -32,51 +33,12 @@ conf <- init.db.global(conf)
 projects.list <- query.projects(conf$con)
 
 ## Temporary hack: restrict to known working projects
-projects.list <- projects.list[projects.list$name%in%c("qemu","git"),]
+#projects.list <- projects.list[projects.list$name%in%c("qemu","git"),]
 
 ## Use the release ranges for the first project in the list
 ## as initial values
 range.ids.list <- query.range.ids.con(conf$con, projects.list$id[[1]])
 #####
-
-annotate.cluster <- function(g) {
-  V(g)$size <- sqrt(V(g)$rankValue*5000)
-  E(g)$width <- sqrt(E(g)$weight)
-
-  ## We store the global properties as attributes of the graph
-  ## to eliminate the need for a second data structure
-
-  ## Reciprocity computes the amount of reciprocal connections. The
-  ## larger the score, the better is bidirectional connection in a graph
-  ## (low score ones tend to be central developer communities with numerous
-  ## unconnected contributors)
-  g$rec <- round(reciprocity(g, ignore.loops=F), digits=3)
-
-  ## TODO: Document the purpose of graph strength
-  g$strength <- mean(graph.strength(g, mode="all"))
-  g$strength <- round(g$strength/vcount(g), digits=3)
-
-  ## Select the most important developers (as per page rank)
-  prank.sorted <- sort(V(g)$rankValue, index.return=T, decreasing=T)
-
-  ## We compute the degree for the three most important developers
-  ## (for large graphs) or of the most important developer for
-  ## small graphs.
-  ## TODO: The threshold is arbitrary
-  if (vcount(g) > 10) {
-    vertex.idx <- prank.sorted$ix[1:3]
-  } else {
-    vertex.idx <- prank.sorted$ix
-  }
-
-  g$deg.graph <- round(mean(degree(g, vertex.idx, normalize=T)), digits=3)
-  g$size <- vcount(g)
-
-  ## TODO: IN the analysis/the clustering phase, also consider the
-  ## page rank distribution and mean page rank.
-
-  return(g)
-}
 
 gen.clusters.list <- function(l, con) {
   clusters.list <- lapply(1:length(l), function(i) {
