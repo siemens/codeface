6c7f58509625862fdd611a1416505bcb71f54315
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-07-12 18:37:49 +0200

    Add functions to compute daily resolved commit statistics
    In contract to the existing time series methods, we are interested
    in computing a summary statistics of commit activities that
    is resolved per-day and per-hour.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- commits.r
+++ commits.r
@@ -17,6 +17,9 @@
 ## Process commit information and create descriptive/visual statistics
 suppressPackageStartupMessages(library(gridExtra))
 suppressPackageStartupMessages(library(corrgram))
+suppressPackageStartupMessages(library(zoo))
+suppressPackageStartupMessages(library(plyr))
+suppressPackageStartupMessages(library(lubridate))
 source("plot.r")
 
 ## Transform commit information data into a form that is easier to visualise
@@ -77,4 +80,42 @@ gen.commits.info <- function(con, pid, range.id) {
   }
 
   return(list(cmt.info=cmt.info, plot.types=plot.types))
+}
+
+## Prepare base data for commits activity calculations
+gen.commits.activity <- function(con, pid, range.id) {
+  subset <- c("commitDate", "DiffSize")
+  cmt.info <- get.commits.by.range.con(con, pid, range.id)
+  cmt.info <- cmt.info[subset]
+
+  return(cmt.info)
+}
+
+## Compute commit activity statistics resolved by week day and hour
+## to get an impression when contributors work on the code
+## Use FUN=sum to get the sum of all diff sizes, and FUN=length to get the
+## number of commits
+compute.hourly.statistics <- function(ts, FUN=length) {
+  wday.labels <- c("Sun", "Mon", "Thu", "Wed", "Thu", "Fri", "Sat")
+  wday.labels.mon <- c("Mon", "Thu", "Wed", "Thu", "Fri", "Sat", "Sun")
+
+  res <- lapply(1:7, function(i) {
+    ts.sub <- ts[wday(ts)==i]
+    if (length(ts.sub) == 0) {
+      return(NULL)
+    }
+    act <- aggregate(coredata(ts.sub), by=list(hour=hour(ts.sub)), FUN)
+    colnames(act) <- c("hour", "size")
+    return(data.frame(day=i, act))
+  })
+
+  res <- do.call(rbind, res)
+
+  ## Convert the labels to use monday as first day of the week
+  res$day <- mapvalues(res$day, 1:7, c(7, 1:6), warn_missing=FALSE)
+  res <- arrange(res, res$day)
+  res$day <- factor(res$day, levels=7:1, ordered=TRUE)
+  res$day <- mapvalues(res$day, 1:7, wday.labels.mon, warn_missing=FALSE)
+
+  return(res)
 }
