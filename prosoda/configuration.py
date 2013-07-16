4d642e29d0b4884ddcc11a2d31af26df9d60d2b8
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-07-16 14:55:45 +0200

    Fix up copyright notices and add missing ones
    Fix copyright notices of files depending on the primary author, and add
    notices to all .py files so any automated tools will not complain.
    
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- prosoda/configuration.py
+++ prosoda/configuration.py
@@ -11,7 +11,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+# Copyright 2013 by Siemens AG, Johannes Ebke <johannes.ebke.ext@siemens.com>
 # All Rights Reserved.
 '''
 Configuration module for prosoda
