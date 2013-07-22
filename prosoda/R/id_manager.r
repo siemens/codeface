b173af32ffa553278aa6de42c7f9e2700e569ba8
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-07-22 16:42:46 +0200

    Add request cache for user ids
    Adds a list to the conf object that caches requests to the REST
    interface about user IDs. If a user ID has been successfully requested
    once, subsequent calls with the same project ID and name will return the
    same user ID without re-requesting it from the REST interface.
    
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- prosoda/R/id_manager.r
+++ prosoda/R/id_manager.r
@@ -19,31 +19,53 @@ suppressPackageStartupMessages(library(RCurl))
 suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(rjson))
 
+query.cache <- list()
+
 query.user.id.base <- function(host, port, pid, name, email) {
   res <- postForm(str_c("http://", host, ":", port, "/post_user_id"),
                   .params=list(projectID=pid, name=name, email=email),
                   style="post", binary=FALSE, .encoding="utf-8")
-
   id <- fromJSON(rawToChar(res))$id
   return(id)
 }
 
-query.user.id <- function(conf, name, email) {
-  return(query.user.id.base(conf$nodejsHostname, conf$nodejsPort, conf$pid,
-                            name, email))
+query.user.id.cached <- function(host, port, pid, name, email) {
+  uid = paste(pid, name, email, sep="\t")
+  if (with(query.cache, exists(uid))) {
+    return(getElement(query.cache, uid))
+  }
+  logdebug(paste("Missed cache for ", uid, "\n"), logger="id_manager")
+  id <- query.user.id.base(host, port, pid, name, email)
+  query.cache[uid] <<- id
+  return(id)
 }
 
+query.user.id <- function(conf, name, email) {
+  return(query.user.id.cached(conf$nodejsHostname, conf$nodejsPort, conf$pid,
+                              name, email))
+}
 
 query.decompose.user.id.base <- function(host, port, pid, name.str) {
   res <- postForm(str_c("http://", host, ":", port, "/post_decompose_user_id"),
                   .params=list(projectID=pid, namestr=name.str),
                   style="post", binary=FALSE, .encoding="utf-8")
-
   id <- fromJSON(rawToChar(res))$id
   return(id)
 }
 
+query.decompose.user.id.cached <- function(host, port, pid, name.str) {
+  uid = paste(pid, name.str, sep="\t")
+  if (with(query.cache, exists(uid))) {
+    return(getElement(query.cache, uid))
+  }
+  logdebug(paste("Missed cache for ", uid, "\n"), logger="id_manager")
+  id <- query.decompose.user.id.base(host, port, pid, name.str)
+  query.cache[uid] <<- id
+  return(id)
+}
+
 query.decompose.user.id <- function(conf, name.str) {
-  return(query.decompose.user.id.base(conf$nodejsHostname, conf$nodejsPort,
-                                      conf$pid, name.str))
+    return(query.decompose.user.id.cached(conf$nodejsHostname,
+                                          conf$nodejsPort,
+                                          conf$pid, name.str))
 }
