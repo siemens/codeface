ea44fb79bb7b857165d72bd6f0131467cfcec97e
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-08-01 09:22:03 +0200

    Add copyright headers to shiny infrastructure files
    Some simple files that are required to instantiate or maintain
    the shiny server infrastructure were not equipped with a
    copyright header. Add a simple permissive license.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- shiny-server-pack.sh
+++ shiny-server-pack.sh
@@ -1,3 +1,12 @@
+#! /bin/sh
+
+# Copyright Siemens AG 2013
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 # This script sets up a self-contained version of the shiny server,
 # downloading a dedicated version of the latest node.js and then
 # installing the modified shiny-server from
--- shiny-server.sh
+++ shiny-server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Copyright Siemens AG 2013
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 export WD=$PWD
 export MYNODEJS=$WD/node-v0.10.13-linux-???/bin/node
 $MYNODEJS $WD/node-v0.10.13-linux-???/lib/node_modules/shiny-server/lib/main.js $WD/shiny-server.config
