242978a0a52ff64c679dd8e1831f224912c5c072
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-09-12 09:14:04 +0200

    Add missing copyright headers
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>

--- prosoda/R/shiny/apps/dashboard-test/gridsterWidgetsExt.r
+++ prosoda/R/shiny/apps/dashboard-test/gridsterWidgetsExt.r
@@ -1,3 +1,18 @@
+# This file is part of prosoda.  prosoda is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2013 by Siemens AG, Albert Eckert <albert.eckert@siemens.com>
+
 gridsterButton <- function(inputId, widgetdialogId) {
   tagList(
     #singleton(tags$head(tags$script(src = "gridsterWidgetsExt.js"))),
