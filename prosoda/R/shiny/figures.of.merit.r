30645ccf730443c61c0c256b8b8c83efa17052c8
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-09-16 15:45:26 +0200

    Add fake figures-of-merit
    Add an initial set of mostly 'fake' figures of merit.
    
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>

--- prosoda/R/shiny/figures.of.merit.r
+++ prosoda/R/shiny/figures.of.merit.r
@@ -13,28 +13,30 @@
 #
 # Copyright 2013 by Siemens AG, Johannes Ebke <johannes.ebke.ext@siemens.com>
 
-## Figures of merit should return a float value from 0 to 1
-
-figure.of.merit.collaboration.warn <- 0.66
-figure.of.merit.collaboration.bad <- 0.33
+figure.of.merit.collaboration.warn <- 0.75
+figure.of.merit.collaboration.bad <- 0.5
 figure.of.merit.collaboration <- function(pid) {
-  return(0.88)
+  n.commits <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM commit WHERE projectId=", pid))[[1]]
+  n.persons <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM person WHERE projectId=", pid))[[1]]
+  #n.issues <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM issue WHERE projectId=", pid))[[1]]
+  min(n.commits/1000., n.persons/20.)
 }
 
-figure.of.merit.communication.warn <- 0.66
-figure.of.merit.communication.bad <- 0.33
+figure.of.merit.communication.warn <- 500
+figure.of.merit.communication.bad <- 100
 figure.of.merit.communication <- function(pid) {
-  return(0.88)
+  n.mail.threads <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM mail_thread WHERE projectId=", pid))[[1]]
 }
 
-figure.of.merit.construction.warn <- 0.66
-figure.of.merit.construction.bad <- 0.33
+figure.of.merit.construction.warn <- 1
+figure.of.merit.construction.bad <- 0
 figure.of.merit.construction <- function(pid) {
-  return(0.88)
+  n.tsplots <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM plots WHERE name LIKE 'Progress%' AND projectId=", pid))[[1]]
+  n.tsplots
 }
 
-figure.of.merit.complexity.warn <- 0.66
-figure.of.merit.complexity.bad <- 0.33
+figure.of.merit.complexity.warn <- 5
+figure.of.merit.complexity.bad <- 0
 figure.of.merit.complexity <- function(pid) {
-  return(0.88)
+  n.understand.plots <- dbGetQuery(conf$con, str_c("SELECT COUNT(*) FROM plots WHERE name LIKE 'Understand%' AND projectId=", pid))[[1]]
 }
