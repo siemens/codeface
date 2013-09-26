b3157cfcb95a5c0a3fa5463d2ec70d61abcff631
Johannes Ebke <johannes.ebke.ext@siemens.com>
2013-09-26 13:04:39 +0200

    Add missing copyright notices
    Add missing copyright notices, extend copyright check to js/css files.
    
    Signed-off-by: Johannes Ebke <johannes.ebke.ext@siemens.com>

--- test_copyright.sh
+++ test_copyright.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for f in $(find prosoda -name '*.py' -or -iname '*.r' -or -iname '*.css' -or -iname '*.js' -or -iname '*.html'); do
-    if [[ -z $(grep "# Copyright " $f) ]]; then 
+    if [[ -z $(grep "Copyright " $f) ]]; then 
         echo "Missing Copyright: $f"
     fi
 done;
