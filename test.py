c033a682741b4a7396ac4d3c7ec0518182bbbd58
Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
2013-10-14 09:51:30 +0200

    Add copyright header to the ID service illustration file
    It's licensed under GPLv2, as usual.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+# This file is part of prosoda.  prosoda is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2013 by Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
+# All Rights Reserved.
 
-# Short python example to test the ID service
+# Short python example to test and illustrate the ID service
 import httplib
 import urllib
 import json
