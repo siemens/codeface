75f60543d763ea1fae3e8d0d46391f4e7ca358be
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2014-01-29 18:46:18 +0100

    Add integration test for network construction correctness
    - None of the existing tests check that the network is being
      constructed correctly from the commit data
    
    - The test will query the database after the analysis pass for
      the network and the person ids, it will then verify that the
      network is correctly linking developers and with the correct
      weight by comparing to known correct network for a given
      set of commits and construction method
    
    - The test will help us to identify any problems with the
      developer ids and the reference to the network, in addition
      to verifying that the entire network construction process
      is functioning properly
    
    - The test is currently only implemented for the proximity
      based analysis, for full coverage we need to calculate
      a ground truth network for the other analysis methods
    
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>
    Reviewed-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>

--- codeface/dbmanager.py
+++ codeface/dbmanager.py
@@ -114,6 +114,27 @@ class DBManager:
             raise Exception("Project id {} not found!".format(pid))
         return self.doFetchAll()[0]
 
+    def get_edgelist(self, cid):
+        self.doExec("SELECT fromId, toId, weight FROM edgelist \
+                    WHERE clusterId={}".format(cid))
+        if self.cur.rowcount == 0:
+            raise Exception("Cluster id {} not found!".format(cid))
+        return self.doFetchAll()
+
+    def get_cluster_id(self, pid):
+        self.doExec("SELECT id FROM cluster WHERE clusterNumber=-1 \
+                    AND projectId={}".format(pid))
+        if self.cur.rowcount == 0:
+            raise Exception("Cluster from project {} not found!".format(pid))
+        return self.doFetchAll()[0][0]
+
+    def get_project_persons(self, pid):
+        self.doExec("SELECT id, name FROM person \
+                    WHERE projectId={}".format(pid))
+        if self.cur.rowcount == 0:
+            raise Exception("Persons from project {} not found!".format(pid))
+        return(self.doFetchAll())
+
     def getTagID(self, projectID, tag, type):
         """Determine the ID of a tag, given its textual form and the type"""
         self.doExec("SELECT id FROM release_timeline WHERE projectId=%s " +
