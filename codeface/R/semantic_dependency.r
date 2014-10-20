a187a0966c90c8e4fb85f2141f5b2072736a9587
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2014-10-20 17:02:28 +0200

    Add file to perform semantic dependency analysis
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

---
+++ codeface/R/semantic_dependency.r
@@ -0,0 +1,91 @@
+library(tm)
+library(lsa)
+
+genDependencyCorpus <- function(con, project.id, start.date, end.date) {
+  depend.df <- query.dependency(con, project.id, "Function", 30, start.date,
+                                end.date, impl=TRUE)
+
+  corp <- Corpus(VectorSource(depend.df[,'impl']))
+
+  return(corp)
+}
+
+
+processCorpus <- function(corp) {
+  ## Handle factors that are specific to
+  ## source code
+  camel_1 <- "(.)([A-Z][a-z]+)"
+  camel_2 <- "([a-z0-9])([A-Z])"
+  removeSoft <- function(x) {
+                   ## split CamelCase
+                   s1 <- gsub(camel_1,"\\1_\\2",x,perl=T)
+                   s2 <- gsub(camel_2,"\\1_\\2",s1,perl=T)
+                   s3 <- gsub("__", "_",s2)
+                   ## Replace underscore with space
+                   s4 <- gsub("_"," ",s3)
+                   ## Repalce non-alphanumeric with space
+                   s5 <- gsub("[^[:alnum:] ]", " ", s4)
+                   return(s5)}
+
+  ct <- content_transformer(removeSoft)
+  corp_mod <- tm_map(corp, ct)
+
+  ## Remove stop words
+  corp_mod <- tm_map(corp_mod, removeWords, stopwords("english"))
+
+  ## Stem words
+  corp_mod <- tm_map(corp_mod, stemDocument)
+
+  return(corp_mod)
+}
+
+
+processTermDocMat <- function(corp) {
+  ## create term document matrix
+  tdm <- TermDocumentMatrix(corp)
+
+  if(length(corp) > 0) {
+    n_docs <- length(corp)
+    max_sparsity <- 1 - (1 / n_docs)
+
+    ## remove terms occuring very infrequently
+    tdm <- removeSparseTerms(tdm, max_sparsity)
+  }
+
+  return(tdm)
+}
+
+
+computeDistMat <- function(tdm) {
+  td.mat <- as.matrix(tdm)
+  ## Perform local and global document weighting
+  td.mat.w <- lw_bintf(td.mat) * gw_idf(td.mat)
+  latent.space <- lsa(td.mat.w)
+  dist.mat <- dist(t(as.textmatrix(latent.space)))
+
+  return(dist.mat)
+}
+
+
+plotMSA <- function(dist.mat){
+  fit <- cmdscale(dist.mat, eig=T, k=2)
+  points <- data.frame(x=fit$points[, 1], y=fit$points[, 2])
+  qplot(x, y, data=points, geom="point", alpha=I(1/5))
+
+  return(points)
+}
+
+
+testAnalysis <- function() {
+  con <- connect.db("../../../codeface.conf")$con
+  p.id <- 2
+  start.date <- "2012-12-10"
+  end.date <- "2013-12-12"
+
+  corp <- genDependencyCorpus(con, p.id, start.date, end.date)
+  corp <- processCorpus(corp)
+  tdm <- processTermDocMat(corp)
+  dist.mat <- computeDistMat(tdm)
+  points <- plotMSA(dist.mat)
+  browser()
+}
