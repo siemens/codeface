0ade50fc9074dde8d7c943d6a15c416dcf45d8fb
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2014-11-19 17:12:43 +0100

    Add ability to construct developer network using co-change concept
    - Co-change coupling is a relationship between two software
      artifacts that change frequently together
    
    - We now have the option to construct a developer network
      where two developers are connected if they contribute
      to software artifacts that have a co-change coupling relationship
    
    - This relationship is in addition to connecting developers that
      make commits a common artifact
    
    - Add test to ensure correct functionality for constructing the
      edgelists using the co-change and common entity contribution
      concepts
    
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/network_stream.r
+++ codeface/R/network_stream.r
@@ -18,16 +18,13 @@ build.dev.net <- function(con, project.id, type, start.date, end.date) {
 }
 
 
-commits.to.edgelist <- function(commit.df, cycle) {
-  commit.df$author <- as.character(commit.df$author)
+entity.group.to.edgelist <- function(entity.groups, cycle) {
+  ## Generate the edges list based on contribution to common entity group
+  edgelist <- generate.person.edgelist(entity.groups)
 
-  ## Group all commits on a common entity
-  entity.group <-aggregate.on.entity(commit.df)
-
-  ## Generation the edges list base on contribution to common
-  ## entity
-  edgelist <- generate.person.edgelist(entity.group)
-  author.id <- unique(commit.df$author)
+  ## Get all authors since some may be isolated and will then not appear in the
+  ## edgelist
+  author.id <- unique(do.call(rbind,entity.groups)$author)
   vertex.data <- data.frame(id=author.id)
 
   return(list(edgelist=edgelist, vertex.data=vertex.data,
@@ -35,11 +32,12 @@ commits.to.edgelist <- function(commit.df, cycle) {
 }
 
 
-build.dev.net.stream <- function(con, project.id, type, dates.df) {
-  ## build a graph stream from a data frame of start and end dates,
+build.dev.net.stream <- function(con, project.id, type, dates.df,
+                                 add.co.change.rel=FALSE) {
+  ## Build a graph stream from a data frame of start and end dates,
   ## similar to build.dev.net except the edge lists for each date range
   ## are generated in parallel
-  limit <- 50
+  limit <- 50  ## Max allowable files edited by a single commit to include
   n.cores <- get.num.cores()
   n.groups <- ceiling(nrow(dates.df) / n.cores)
   g.indx <- as.vector(sapply(1:n.groups, function(i) rep(i, n.cores)))
@@ -49,18 +47,41 @@ build.dev.net.stream <- function(con, project.id, type, dates.df) {
   network.stream <-
     lapply(df.sets,
       function(df) {
-      # Query db for commit sets, can't be done in parallel
-      commit.sets <- apply(df, 1, function(r) {
+        ## Query db for commit sets, can't be done in parallel
+        commit.lists <- apply(df, 1, function(r) {
         start.date <- r['start.date']
         end.date <- r['end.date']
-        query.dependency(con, project.id, type, limit, start.date,
-                         end.date)})
+        commit.df <- query.dependency(con, project.id, type, limit,
+                                      start.date, end.date)
 
-      names(commit.sets) <- paste(df$start.date, df$end.date, sep="-")
+        ## Co-change relationship needs a longer history of commits
+        commit.df.hist <- list()
+        if(add.co.change.rel) {
+          ## Get co-change relationships for previous and current commits with
+          ## a limit for the history to consider
+          historical.limit <- ddays(365)
+          start.date.hist <- as.Date(start.date) - historical.limit
+          end.date.hist <- start.date
+          commit.df.hist <- query.dependency(con, project.id, type, limit,
+                                             start.date.hist, end.date.hist)
 
-      mclapply(names(commit.sets),
-        function(cycle) commits.to.edgelist(commit.sets[[cycle]], cycle),
-        mc.cores=n.cores)
+          ## Add commit.df because that is relavent also but no point in querying
+          ## the database again since we already have that date range
+          commit.df.hist <- rbind(commit.df, commit.df.hist)
+        }
+
+        cycle <- paste(start.date, end.date, sep="-")
+
+        res <- list(commit.df=commit.df, commit.df.hist=commit.df.hist,
+                    cycle=cycle)
+
+        return(res)})
+
+      edgelist <-
+        mclapply(commit.lists,
+                 function(commit.list) construct.edgelist(commit.list,
+                                                          add.co.change.rel),
+                 mc.cores=n.cores)
       })
 
   network.stream <- unlist(network.stream, recursive=FALSE, use.names=FALSE)
@@ -70,12 +91,74 @@ build.dev.net.stream <- function(con, project.id, type, dates.df) {
 }
 
 
-aggregate.on.entity <- function(commit.df) {
-    ## Group rows that make a change to a common entity
-    commit.df$entity <- as.factor(commit.df$entity)
-    entity.group <- split(commit.df, commit.df$entity)
+construct.edgelist <- function(commit.list, add.co.change.rel) {
+  ## Compute relation for developer contribution to common entity
+  entity.groups <- aggregate.on.common.entity(commit.list$commit.df)
 
-    return(entity.group)
+  ## Compute relation for developers contribution to co-change coupled
+  ## entities
+  if(add.co.change.rel) {
+    ## Add the co-change relation to the entity grouping
+    entity.groups <- add.co.change.aggregation(commit.list$commit.df.hist,
+                                               entity.groups)
+  }
+  ## Convert grouped entites into developer network, edges are
+  ## placed between developers who have made a contribution to
+  ## common entity group
+  edgelist <- entity.group.to.edgelist(entity.groups, commit.list$cycle)
+
+  return(edgelist)
+}
+
+
+aggregate.on.common.entity <- function(commit.df) {
+    ## Group rows that make a change to a common entity
+    entity.factor <- as.factor(commit.df$entity)
+    entity.group <- split(commit.df, entity.factor)
+
+  return(entity.group)
+}
+
+
+add.co.change.aggregation <- function(commit.df, entity.group) {
+  ## Get list of currently changed entities, then we can use this information
+  ## to eliminate the historical relationships that are not relavent to the current
+  ## development
+  relavent.entity.list <- unique(do.call(rbind, entity.group)$entity)
+  names(relavent.entity.list) <- relavent.entity.list
+
+  ## Remove non-relavent entities from the historical commits which fall outside
+  ## of the current development window
+  commit.df <- commit.df[commit.df$entity %in% relavent.entity.list, ]
+
+  ## Compute co-change relationship using frequent item set mining
+  freq.item.sets <- compute.frequent.items(commit.df)
+  ## Compute an edgelist from the frequent item sets
+  co.change.edgelist <- compute.item.sets.edgelist(freq.item.sets)
+
+  ## If co-change relationships are identified then add them
+  ## Create a mapping of entities to their co-changed entities
+  co.change.map <-
+    lapply(relavent.entity.list,
+           function(entity) {
+             ## Find co-change relationships for this entity
+             col.1.entity.match <- co.change.edgelist$X1 == entity
+             col.2.entity.match <- co.change.edgelist$X2 == entity
+             co.change.entities <- union(co.change.edgelist[col.1.entity.match, "X2"],
+                                         co.change.edgelist[col.2.entity.match, "X1"])
+             return(co.change.entities)})
+
+  ## Group entity groups that have a co-change relationship
+  entity.group <-
+    lapply(names(entity.group),
+           function(entity.name) {
+             entity.co.change.group <- co.change.map[[entity.name]]
+             df.list <- entity.group[entity.co.change.group]
+             ## Combine data frames
+             df.co.change <- do.call(rbind, df.list)
+             res <- rbind(df.co.change, entity.group[[entity.name]])})
+
+  return(entity.group)
 }
 
 
