769efc8bb2a5dcabb30fdc0f4d9e9ee31daffe38
Matthias Dittrich <matthi.d@gmail.com>
2014-12-08 19:02:02 +0100

    Added integration tests for feature and feature_file analysis.
    This adds various integration tests for the feature and feature_file analysis.
    We also add a new integration setup where no mailing list analysis is run.
    This setup can be used by other analysis (but they are commented in this commit because they do not work currently).
    
    Signed-off-by: Matthias Dittrich <matthi.d@gmail.com>

---
+++ codeface/test/integration/test_features.py
@@ -0,0 +1,198 @@
+# This file is part of Codeface. Codeface is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2013 by Matthias Dittrich <matthi.d@gmail.com>
+# All Rights Reserved.
+
+import unittest
+from codeface.test.integration.example_projects import example_project_func
+from codeface.test.integration.test_exampleprojects \
+    import EndToEndTestSetup
+
+__author__ = 'Matthias Dittrich'
+
+
+class EndToEndOnlyTaggingTestSetup(EndToEndTestSetup):
+
+    def setup_with_p(self, p):
+        EndToEndTestSetup.setup_with_p(self, p)
+
+        self.add_ignored_tables(
+            ["mail_thread", "mailing_list", "twomode_vertices",
+             "initiate_response", "freq_subjects", "twomode_edgelist",
+             "thread_responses"])
+
+    pass
+
+
+class TestEndToEndOnlyTagging(object):
+    def testEndToEnd(self):
+        self.p = example_project_func[self.example_project](self.tagging)
+        with self.p:
+            self.setup_with_p(self.p)
+            self.analyseEndToEnd()
+            if self.correct_edges:
+                self.checkEdges()
+            self.checkResult()
+            self.checkClean()
+
+
+#class TestEndToEndOnlyTaggingExample1Tag(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 1
+#    tagging = "tag"
+#    correct_edges = None
+#
+#
+#class TestEndToEndOnlyTaggingExample1C2A(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 1
+#    tagging = "committer2author"
+#    correct_edges = None
+#
+#
+#class TestEndToEndOnlyTaggingExample1Proximity(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 1
+#    tagging = "proximity"
+#    add_ignore_tables = ["edgelist"]
+#    correct_edges = None
+
+#class TestEndToEndOnlyTaggingExample1Feature(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 1
+#    tagging = "feature"
+#    add_ignore_tables = ["edgelist"]
+#    correct_edges = None
+#
+#
+#class TestEndToEndOnlyTaggingExample1Feature_File(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 1
+#    tagging = "feature_file"
+#    add_ignore_tables = ["edgelist"]
+#    correct_edges = None
+#
+#
+#class TestEndToEndOnlyTaggingExample2Feature(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 2
+#    tagging = "feature"
+#    add_ignore_tables = ["edgelist"]
+#    devs = ["Louie Loner", "Geoff Genius", "Bill Bully", "Max Maintainer",
+#            "Adam Awkward", "Peter Popular", "Clara Confident"]
+#    correct_edges = []
+#
+#
+#class TestEndToEndOnlyTaggingExample2Feature_File(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 2
+#    tagging = "feature_file"
+#    add_ignore_tables = ["edgelist"]
+#    devs = ["Louie Loner", "Geoff Genius", "Bill Bully", "Max Maintainer",
+#            "Adam Awkward", "Peter Popular", "Clara Confident"]
+#    correct_edges = []
+
+
+class TestEndToEndOnlyTaggingExample3Feature(
+        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+    example_project = 3
+    tagging = "feature"
+    add_ignore_tables = ["edgelist"]
+    devs = ["Louie Loner", "Geoff Genius", "Bill Bully", "Max Maintainer",
+            "Adam Awkward", "Peter Popular", "Clara Confident"]
+
+    correct_edges =\
+        [   # Release 0 - Release 1
+            # Explanation of the data:
+            # You can see the file-state in the last commit, the comments
+            # help to identify where the line comes from (blame analysis).
+            # As we use the last state to calculate collaboration, we can
+            # see that peter, bill and adam worked on the features A and B.
+            # So they have a collaboration with each other
+            [
+                [devs[2], devs[5], 1.0],  # bill and peter
+                [devs[5], devs[2], 1.0],  # peter and billy
+                [devs[2], devs[4], 1.0],  # bill and adam
+                [devs[4], devs[2], 1.0],  # adam and bill
+                [devs[5], devs[4], 1.0],  # peter and adam
+                [devs[4], devs[5], 1.0]],  # adam and peter
+            # Release 1 - Release 2
+            # Here is a list of the changes grouped by features:
+            # Feature 'A':
+            # Adam:  4
+            # Clara:  3
+            # Geoff: 1
+            #
+            # Feature 'B':
+            # Clara: 2
+            # Adam: 3
+            #
+            # Feature 'C':
+            # Adam: 2
+            # Max: 1
+            # The numbers represent the number of changed lines.
+            [   # Feature A
+                [devs[4], devs[6], 1.0],  # adam and clara
+                [devs[6], devs[4], 1.0],
+                [devs[4], devs[1], 1.0],  # adam and geoff
+                [devs[1], devs[4], 1.0],
+                [devs[6], devs[1], 1.0],  # clara and geoff
+                [devs[1], devs[6], 1.0],
+                # Feature B
+                # clara and adam (already in Feature A)
+                # Feature C
+                # Adam and max
+                [devs[4], devs[3], 1.0],  # adam and max
+                [devs[3], devs[4], 1.0]]
+        ]
+
+
+class TestEndToEndOnlyTaggingExample3Feature_File(
+        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+    example_project = 3
+    tagging = "feature_file"
+    add_ignore_tables = ["edgelist"]
+    devs = ["Louie Loner", "Geoff Genius", "Bill Bully", "Max Maintainer",
+            "Adam Awkward", "Peter Popular", "Clara Confident"]
+
+    correct_edges =\
+        [   # Release 0 - Release 1
+            # Explanation of the data:
+            # The commits are analysed by taking their order into account:
+            # (see also 'feature' analysis)
+            [   # peter and adam (peter committed after adam)
+                [devs[5], devs[4], 1.0],
+                # peter and bill
+                # (peter committed after bill, see commit date)
+                [devs[5], devs[2], 1.0],
+                # bill and adam
+                [devs[2], devs[4], 1.0]],
+            # Release 1 - Release 2
+            # From the "src/carp.c" file we get no collaboration as there
+            # was only one commit within this file
+            # (within the current commit range)!
+            # In the other file we can see on the last state that
+            # Clara collaborated with Adam on Feature A and Feature B
+            # and Max with Adam on Feature C
+            [   # clara and adam (clara after adam)
+                [devs[6], devs[4], 1.0],
+                # max and adam (max after adam)
+                [devs[3], devs[4], 1.0]]]
+
+#class TestEndToEndOnlyTaggingExample2Tag(
+#        EndToEndOnlyTaggingTestSetup, TestEndToEndOnlyTagging):
+#    example_project = 2
+#    tagging = "tag"
+#    correct_edges = None
+#    #testEndToEnd = unittest.expectedFailure(TestEndToEndOnlyTagging.testEndToEnd)
