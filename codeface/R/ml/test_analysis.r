a07f875480070664451d1f80d808221306f9d069
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2015-07-28 14:58:19 +0200

    Add basic test for email analysis
    - We currently have no tests here and package updates are
      constantly breaking things
    
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

---
+++ codeface/R/ml/test_analysis.r
@@ -0,0 +1,19 @@
+library(testthat)
+
+source("analysis.r")
+
+conf <- list(listname="gmane.comp.emulators.qemu")
+path <- "test_data"
+res.dir <- "test_data"
+
+test.genforest <- function() {
+  corp <- gen.forest(conf, path, res.dir)
+  file.remove(file.path(res.dir, paste("corp.base", conf$listname, sep=".")))
+  check.eq <- unlist(meta(corp$corp.orig, tag="datetimestamp")) == unlist(meta(corp$corp, tag="datetimestamp"))  
+  return(all(check.eq))
+}
+
+test_that("Forest generation functions correctly", {
+            expect_true(test.genforest())
+          })
+
