7b08d919eb56ec62dcce88f5811569d84555c610
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2015-07-29 18:27:33 +0200

    Add tests for mailing list analysis
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/ml/test_analysis.r
+++ codeface/R/ml/test_analysis.r
@@ -13,7 +13,18 @@ test.genforest <- function() {
   return(all(check.eq))
 }
 
+test.check.corpus.precon <- function() {
+  corp <- gen.forest(conf, path, res.dir)
+  file.remove(file.path(res.dir, paste("corp.base", conf$listname, sep=".")))
+  corp <- check.corpus.precon(corp)
+  check.eq <- unlist(meta(corp$corp.orig, tag="datetimestamp")) == unlist(meta(corp$corp, tag="datetimestamp"))
+  return(all(check.eq))
+}
+
 test_that("Forest generation functions correctly", {
             expect_true(test.genforest())
           })
 
+test_that("Corpus precondiction checks works", {
+            expect_true(test.check.corpus.precon())
+          })
