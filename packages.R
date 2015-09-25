ff42084e9d74f104c358a88748a8e678f02acd8a
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2015-09-26 01:29:35 +0200

    Fix indentation in packages.R
    According to our style guides, spaces are used
    for indenting R code instead of tabs. Fix this for
    packages.R.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

--- packages.R
+++ packages.R
@@ -1,15 +1,15 @@
 filter.installed.packages <- function(packageList)  {
-	if("-f" %in% commandArgs(trailingOnly = TRUE)) {
-		return(packageList)
-	} else {
-		return(packageList[which(packageList %in% installed.packages()[,1] == FALSE)])
-	}
+    if("-f" %in% commandArgs(trailingOnly = TRUE)) {
+        return(packageList)
+    } else {
+        return(packageList[which(packageList %in% installed.packages()[,1] == FALSE)])
+    }
 }
 
 p <- filter.installed.packages(c("BiRewire", "graph", "Rgraphviz"))
 if(length(p) > 0) {
-	source("http://bioconductor.org/biocLite.R")
-	biocLite(p)
+    source("http://bioconductor.org/biocLite.R")
+    biocLite(p)
 }
 
 p <- filter.installed.packages(c("statnet", "ggplot2", "tm", "optparse",
@@ -19,17 +19,17 @@ p <- filter.installed.packages(c("statnet", "ggplot2", "tm", "optparse",
                    "RCurl", "mgcv", "shiny", "dtw", "httpuv", "devtools",
                    "corrgram", "logging", "png", "rjson", "lsa", "RJSONIO"))
 if(length(p) > 0) {
-	install.packages(p ,dependencies=T)
+    install.packages(p ,dependencies=T)
 }
 
 
 p <- filter.installed.packages(c("snatm", "tm-plugin-mail"))
 if(length(p) > 0) {
-	install.packages(p, repos="http://R-Forge.R-project.org")
+    install.packages(p, repos="http://R-Forge.R-project.org")
 }
 
 
 p <- filter.installed.packages(c("shinyGridster"))
 if(length(p) > 0) {
-	devtools::install_github("wch/shiny-gridster")
+    devtools::install_github("wch/shiny-gridster")
 }
