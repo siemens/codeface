0092cd60a1347d9c4b57ab593af8164b0337c4ff
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2015-10-07 14:14:49 +0200

    Change line endings
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- datamodel/codeface_schema.sql
+++ datamodel/codeface_schema.sql
@@ -1,7 +1,16 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema codeface
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema codeface
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `codeface` DEFAULT CHARACTER SET utf8 ;
 USE `codeface` ;
 
