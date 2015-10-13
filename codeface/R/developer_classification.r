f36d8cddb065e0d5c13a179d22e42a33cad2cf70
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2015-10-13 11:45:00 +0200

    Add to developer classification analysis
    - Add comparison between classifications
    
    - New functions for computing markov chain for different
      classification scheme
    
    - Add to graph stream analysis
    
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/developer_classification.r
+++ codeface/R/developer_classification.r
@@ -1,4 +1,5 @@
 suppressMessages(library(igraph))
+suppressMessages(library(markovchain))
 
 source("db.r")
 source("query.r")
@@ -19,15 +20,14 @@ get.developer.class.con <- function(con, project.id, start.date, end.date) {
 
 ## Low-level function to compute classification
 get.developer.class <- function(author.commit.count, threshold=0.8) {
-  author.commit.count <- author.commit.count[order(-author.commit.count$freq),]
-  num.commits <- sum(author.commit.count$freq)
+  developer.class <- author.commit.count[order(-author.commit.count$freq),]
+  num.commits <- sum(developer.class$freq)
   commit.threshold <- round(threshold * num.commits)
-  core.test <- cumsum(author.commit.count$freq) < commit.threshold
-  core.developers <- author.commit.count[core.test,]
-  peripheral.developers <- author.commit.count[!core.test,]
-  res <- list(core=core.developers, peripheral=peripheral.developers)
+  core.test <- cumsum(developer.class$freq) < commit.threshold
+  developer.class[core.test, "class"] <- "core"
+  developer.class[!core.test, "class"] <- "peripheral"
 
-  return(res)
+  return(developer.class)
 }
 
 ## Determine developer class based on vertex centrality
@@ -36,13 +36,40 @@ get.developer.class.centrality <- function(edgelist, vertex.ids, threshold=0.8,
   graph <- graph.data.frame(edgelist, directed=TRUE,
                             vertices=data.frame(vertex.ids))
   centrality.vec <- sort(FUN(graph), decreasing=T)
-  centrality.df <- data.frame(author=names(centrality.vec),
-                              centrality=as.vector(centrality.vec))
+  developer.class <- data.frame(author=names(centrality.vec),
+                                centrality=as.vector(centrality.vec))
   centrality.threshold <- threshold * sum(centrality.vec)
-  core.test <- cumsum(centrality.vec) < centrality.threshold
-  core.developers <- centrality.df[core.test,]
-  peripheral.developers <- centrality.df[!core.test,]
-  res <- list(core=core.developers, peripheral=peripheral.developers)
+  core.test <- cumsum(developer.class$centrality) < centrality.threshold
+  developer.class[core.test, "class"] <- "core"
+  developer.class[!core.test, "class"] <- "peripheral"
 
-  return(res)
+  return(developer.class)
+}
+
+
+## Compare aggreement between developer classes
+compare.classification <- function(developer.class.1, developer.class.2) {
+  classes.merged <- merge(developer.class.1, developer.class.2, by="author")
+  classes.merged$match <- classes.merged$class.x == classes.merged$class.y
+  percent.match <- nrow(subset(classes.merged, match==T)) / nrow(classes.merged)
+  return(percent.match)
+}
+
+
+## Compute markov chain for developer class transitions
+compute.class.markov.chain <- function(developer.class.list) {
+  index <- names(developer.class.list)
+  turnover <-
+      lapply(index,
+          function(t) {
+            class.state.t <- developer.class.list[[t]]
+            res <- class.state.t[, c("author", "class")]
+            state.t <- paste("state.", t, sep="")
+            colnames(res) <- c("id", state.t)
+            return(res)
+          })
+  turnover.all <- join_all(turnover, by="id", type="full")
+  turnover.all[is.na(turnover.all)] <- "absent"
+  markov.chain <- markovchainFit(data=turnover.all[, -1], method="mle")$estimate
+  return(markov.chain)
 }
--- codeface/R/test_developer_classification.r
+++ codeface/R/test_developer_classification.r
@@ -8,7 +8,8 @@ get.developer.class.test <- function() {
   commit.df <- data.frame(author=sample(1:50, size=sample.size, replace=T))
   author.commit.count <- count(commit.df, "author")
   developer.class <- get.developer.class(author.commit.count, threshold)
-  res <- sum(developer.class$core$freq) < threshold*sample.size
+  core.class <- subset(developer.class, class=="core")
+  res <- sum(core.class$freq) < threshold*sample.size
   return(res)
 }
 
@@ -18,8 +19,23 @@ get.developer.class.centrality.test <- function() {
   edgelist <- get.data.frame(g)
   vertex.ids <- c(as.vector(V(g)), 301:305)
   developer.class <- get.developer.class.centrality(edgelist, vertex.ids,
-                                                    threshold, degree)
-  res <- sum(developer.class$core$centrality) < threshold*sum(degree(g))
+                                                    threshold)
+  core.class <- subset(developer.class, class=="core")
+  res <- sum(core.class$centrality) < threshold*sum(igraph::degree(g))
+  return(res)
+}
+
+compare.classification.test <- function() {
+  g <- barabasi.game(300)
+  edgelist <- get.data.frame(g)
+  vertex.ids <- c(as.vector(V(g)))
+  degree.vec <- igraph::degree(g)[sample(seq_along(vertex.ids), length(degree.vec))]
+  commits <- unlist(sapply(vertex.ids, function(id) rep(id, degree.vec[id])))
+  commit.df <- data.frame(author=commits)
+  class.centrality <- get.developer.class.centrality(edgelist, vertex.ids)
+  class.commit <- get.developer.class(commit.df)
+  class.match <- compare.classification(class.centrality, class.commit)
+  res <- class.match > 0 & class.match < 1
   return(res)
 }
 
@@ -29,4 +45,8 @@ test_that("get.developer.class returns expected values", {
 
 test_that("get.developer.class.centrality returns expected values", {
       expect_true(get.developer.class.centrality.test())
+    })
+
+test_that("compare.classification returns expected values", {
+      expect_true(compare.classification.test())
     })
