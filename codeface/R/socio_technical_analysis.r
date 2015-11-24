197022108da3bcf54d358a88cca37acb6015e747
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2015-11-24 14:46:05 +0100

    Integrate dsm based dependencies into the socio technical analysis
    - We can now perform the analysis using the static dependencies
      provided by Rick's DSMs
    
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/socio_technical_analysis.r
+++ codeface/R/socio_technical_analysis.r
@@ -5,6 +5,7 @@ library("BiRewire")
 source("query.r")
 source("config.r")
 source('dependency_analysis.r')
+source('process_dsm.r')
 
 plot.to.file <- function(g, outfile) {
   g <- simplify(g,edge.attr.comb="first")
@@ -58,9 +59,11 @@ preprocess.graph <- function(g) {
 
 ## Configuration
 if (!exists("conf")) conf <- connect.db("../../codeface.conf")
+dsm.filename <- "/home/mitchell/Downloads/cassandra-2.1.0.dsm.xlsx"
 con <- conf$con
 project.id <- 2
-artifact.type <- "function"
+artifact.type <- list("function", "file")[[2]]
+dependency.type <- list("co-change", "dsm")[[2]]
 person.role <- "developer"
 start.date <- "2015-07-01"
 end.date <- "2015-10-01"
@@ -77,17 +80,28 @@ mail.dat <- query.mail.edgelist(con, project.id, start.date, end.date)
 mail.dat[, c(1,2)] <- sapply(mail.dat[, c(1,2)], as.character)
 
 ## Compute entity-entity relations
-start.date.hist <- as.Date(start.date) - historical.limit
-end.date.hist <- start.date
-commit.df.hist <- query.dependency(con, project.id, artifact.type, file.limit,
-                                   start.date.hist, end.date.hist)
 relavent.entity.list <- unique(vcs.dat$entity)
-commit.df.hist <- commit.df.hist[commit.df.hist$entity %in% relavent.entity.list, ]
+if (dependency.type == "co-change") {
+  start.date.hist <- as.Date(start.date) - historical.limit
+  end.date.hist <- start.date
 
-## Compute co-change relationship
-freq.item.sets <- compute.frequent.items(commit.df.hist)
-## Compute an edgelist
-co.change.dat <- compute.item.sets.edgelist(freq.item.sets)
+  commit.df.hist <- query.dependency(con, project.id, artifact.type, file.limit,
+                                     start.date.hist, end.date.hist)
+
+  commit.df.hist <- commit.df.hist[commit.df.hist$entity %in% relavent.entity.list, ]
+
+  ## Compute co-change relationship
+  freq.item.sets <- compute.frequent.items(commit.df.hist)
+  ## Compute an edgelist
+  dependency.dat <- compute.item.sets.edgelist(freq.item.sets)
+  names(dependency.dat) <- c("V1", "V2")
+
+} else if (dependency.type == "dsm") {
+  dependency.dat <- load.dsm.edgelist(dsm.filename)
+  dependency.dat <-
+    dependency.dat[dependency.dat[, 1] %in% relavent.entity.list &
+                   dependency.dat[, 2] %in% relavent.entity.list, ]
+}
 
 ## Compute node sets
 node.function <- unique(vcs.dat$entity)
@@ -110,10 +124,11 @@ g.bipartite <- add.edges(g.nodes, vcs.edgelist)
 g <- graph.empty(directed=FALSE)
 mail.edgelist <- as.character(with(mail.dat, ggplot2:::interleave(from, to)))
 g <- add.edges(g.bipartite, mail.edgelist, attr=list(color="red"))
-browser()
+
 ## Add entity-entity edges
-co.change.edgelist <- as.character(with(co.change.dat, ggplot2:::interleave(X1, X2)))
-g <- add.edges(g, co.change.edgelist)
+dependency.edgelist <- as.character(with(dependency.dat,
+                                         ggplot2:::interleave(V1, V2)))
+g <- add.edges(g, dependency.edgelist)
 
 ## Apply filters
 g <- preprocess.graph(g)
