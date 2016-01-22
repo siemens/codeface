cb28a8f0592484acade0c4adc2d20096bcd0b346
Mitchell Joblin <mitchell.joblin.ext@siemens.com>
2016-01-22 16:41:41 +0100

    Fix up corrective commit retrieval
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/quality_analysis.r
+++ codeface/R/quality_analysis.r
@@ -2,13 +2,20 @@ source("db.r")
 
 get.corrective.count <- function(con, project.id, start.date, end.date,
                                  entity.type) {
+  entity.type <- tolower(entity.type)
+  if (entity.type=="file") {
+    entity.type.db <- "function"
+    group.by <- "GROUP BY file"
+  } else if (entity.type=="function") {
+    group.by <- "GROUP BY file, entityId"
+  }
 
   query <- str_c("SELECT file, entityId, entityType,
                   SUM(corrective) as corrective, COUNT(*) as total",
                  "FROM commit_dependency, commit",
                  "WHERE commit.id = commit_dependency.commitId",
                  "AND commit.projectId=", project.id,
-                 "AND commit_dependency.entityType=", sq(entity.type),
+                 "AND commit_dependency.entityType=", sq(entity.type.db),
                  "AND commit.commitDate >=", sq(start.date),
                  "AND commit.commitDate <", sq(end.date),
                  "GROUP BY file, entityId", sep=" ")
@@ -16,8 +23,15 @@ get.corrective.count <- function(con, project.id, start.date, end.date,
   dat <- dbGetQuery(con, query)
   dat$norm.corrective <- dat$corrective / dat$total
 
+  if(nrow(dat) > 0) {
+    if (entity.type == "function") {
+      dat$entity <- apply(dat[,cols], 1, paste, collapse="/")
+    } else if (entity.type == "file") {
+      dat$entity <- dat[, "file"]
+    }
+  }
+
   cols <- c('file', 'entityId')
-  dat$entity <- apply(dat[,cols], 1, paste, collapse="/")
   dat <- dat[, !names(dat) %in% cols]
 
   return(dat)
