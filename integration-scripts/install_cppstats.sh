d8aebb7189471fdfb763926bea7461afc4ccd8d0
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2016-06-16 14:35:22 +0200

    Provision cppstats and srcML outside git tree
    Do not clutter the repository with temporary external data
    when installing supplementary tools. Instead, use
    proper random subdirectories in /tmp. See 105d5315c1 for
    additional reasons.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

--- integration-scripts/install_cppstats.sh
+++ integration-scripts/install_cppstats.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+# NOTE: bash instead of sh is required for pushd/popd
 
 export CPPSTATS_VERSION=0.8.4
 
 echo "Providing cppstats $CPPSTATS_VERSION"
 
-mkdir -p vendor/
-cd vendor/
+pushd .
+TMPDIR=`mktemp -d` || exit 1
+cd ${TMPDIR}
 
-wget --quiet https://codeload.github.com/clhunsen/cppstats/tar.gz/v$CPPSTATS_VERSION -O /tmp/cppstats.tar.gz
-tar -xvf /tmp/cppstats.tar.gz
-export CPPSTATS=$PWD/cppstats-$CPPSTATS_VERSION/
-echo '#!/bin/bash' > $CPPSTATS/cppstats
-echo "cd $CPPSTATS" >> $CPPSTATS/cppstats
-echo "PYTHONPATH=\"\$PYTHONPATH:$CPPSTATS/lib\" ./cppstats.py \"\$@\"" >> $CPPSTATS/cppstats
-chmod +x $CPPSTATS/cppstats
+CPPSTATS_URL="https://codeload.github.com/clhunsen/cppstats/tar.gz/v${CPPSTATS_VERSION}"
+wget --quiet ${CPPSTATS_URL} -O ${TMPDIR}/cppstats.tar.gz
+if [ ! -e ${TMPDIR}/cppstats.tar.gz ]
+then
+    echo "Could not download cppstats from ${CPPSTATS_URL}"
+    exit 1
+fi
+(cd /opt; sudo tar -xvf ${TMPDIR}/cppstats.tar.gz)
+export CPPSTATS=/opt/cppstats-$CPPSTATS_VERSION/
+echo '#!/bin/bash' > ${TMPDIR}/cppstats
+echo "cd $CPPSTATS" >> ${TMPDIR}/cppstats
+echo "PYTHONPATH=\"\$PYTHONPATH:/opt/$CPPSTATS/lib\" ./cppstats.py \"\$@\"" >> ${TMPDIR}/cppstats
+chmod +x ${TMPDIR}/cppstats
+sudo cp ${TMPDIR}/cppstats /usr/local/bin/cppstats
 
 echo "Providing srcML"
 SRCML_URL="http://131.123.42.38/lmcrs/srcML-Ubuntu14.04-64.tar.gz"
@@ -24,8 +33,6 @@ then
     exit 1
 fi
 tar -xvf ${TMPDIR}/srcML.tar.gz
-cp -rf $PWD/srcML/* $CPPSTATS/lib/srcml/linux/
+sudo cp -rf $PWD/srcML/* $CPPSTATS/lib/srcml/linux/
 
-sudo ln -sf $CPPSTATS/cppstats /usr/local/bin/cppstats
-
-cd ..
+popd
