b076a29c14a110d27b22b083cfd2f15629c91d32
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2016-06-16 18:22:52 +0200

    Provide license headers for integration files
    Refactoring the vagrant file did not pull over copyright
    headers. Integrate the information back.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
    CC: Roger Meier <roger@bufferoverflow.ch>
    CC: Matthias Dittrich <matthi.d@gmail.com>
    CC: Claus Hunsen <hunsen@fim.uni-passau.de>

--- integration-scripts/test_codeface.sh
+++ integration-scripts/test_codeface.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright Matthias Dittrich <matthi.d@gmail.com>
+# SPDX-License-Identifier:	Apache-2.0 BSD-2-Clause GPL-2.0+ MIT WTFPL
 
 cd "id_service"
 node id_service.js ../codeface.conf &
