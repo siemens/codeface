128090c668d10f9055a7edd8fc93a97b7a5649fa
Claus Hunsen <hunsen@fim.uni-passau.de>
2016-07-13 13:13:48 +0200

    Move cppstats to most recent version (v0.9.2)
    Due to a change in the installation routine of cppstats (URL to python-
    statlib), the cppstats version for Codeface is changed to v0.9.2 to
    ensure proper integration tests.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- integration-scripts/install_cppstats.sh
+++ integration-scripts/install_cppstats.sh
@@ -5,7 +5,7 @@
 # Copyright Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
 # SPDX-License-Identifier:	Apache-2.0 BSD-2-Clause GPL-2.0+ MIT WTFPL
 
-export CPPSTATS_VERSION=0.9.1
+export CPPSTATS_VERSION=0.9.2
 
 echo "Providing cppstats $CPPSTATS_VERSION"
 
