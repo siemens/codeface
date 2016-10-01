d01e9b53765ad5e3e3e64b417e39c423b2de2140
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2016-09-30 21:09:40 -1000

    Remove support for reading Titan DSMs in Excel format
    The Titan toolchain can produce DSMs in multiple formats. Since their
    content is identical, it suffices to read the plain text variant.
    Eliminate support for reading the Excel equivalent, since this
    allows us to get rid of some package dependencies that are only
    required for this purpose.
    
    Use the opportunity to equip the file with license information.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
    Signed-off-by: Mitchell Joblin <mitchell.joblin.ext@siemens.com>

--- codeface/R/process_dsm.r
+++ codeface/R/process_dsm.r
@@ -1,56 +1,24 @@
+#! /usr/bin/env Rscript
+## This file is part of Codeface. Codeface is free software: you can
+## redistribute it and/or modify it under the terms of the GNU General Public
+## License as published by the Free Software Foundation, version 2.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+##
+## Copyright 2016, Siemens AG, Mitchell Joblin <mitchell.joblin.ext@siemens.com>
+## All Rights Reserved.
+
+## Support routines for handling DSMs produced by the Titan toolset
+
 s <- suppressPackageStartupMessages
-s(library(XLConnect))
-s(library(readxl))
 s(library(igraph))
-
-## Load a DSM matrix in XLS format as produced by the Titan toolchain
-load.dsm.edgelist <- function(dsm.filname, relavent.files) {
-    ## Load excel file
-    dsm <- read_excel(dsm.filename, sheet="Sheet0", col_names=FALSE)
-    dsm[1,] <- as.integer(dsm[1,])
-    ##wb <- loadWorkbook(dsm.filename)
-    ##dsm <- readWorksheet(wb, sheet="Sheet0", header=F)
-
-    ## First column is an index and second is the filename
-    file.col.num <- 2
-    dsm.vertices <- dsm[, file.col.num]
-    names(dsm.vertices) <- dsm[, 1]
-    dsm.vertices <- sapply(dsm.vertices,
-                           function(filename) {
-                               filename <- gsub("_", ".", filename, fixed=T)
-                               return(filename)})
-
-    ## Remove files that have not been edited
-    keep.file <- dsm.vertices %in% relavent.files
-    keep.file[1] <- TRUE # keep index row
-    dsm <- dsm[c(keep.file), ]
-    dsm <- dsm[, c(T, keep.file)]
-
-    ## Drop filename column
-    dsm <- dsm[, -file.col.num]
-
-    ## Assign matrix row/col names
-    dsm.matrix <- as.matrix(dsm)
-    rownames(dsm.matrix) <- sapply(dsm[, 1],
-                                   function(i) dsm.vertices[as.character(i)])
-    colnames(dsm.matrix) <- sapply(dsm[1 ,],
-                                   function(i) dsm.vertices[as.character(i)])
-
-    ## Remove row/col index
-    dsm.matrix <- dsm.matrix[-1, ]
-    dsm.matrix <- dsm.matrix[, -1]
-
-    ## Convert string elements to integers
-    dsm.matrix[!is.na(dsm.matrix)] <- 1
-    dsm.matrix[is.na(dsm.matrix)] <- 0
-    diag(dsm.matrix) <- 0
-
-    ## Convert to edge list
-    dsm.graph <- graph.adjacency(dsm.matrix, mode="undirected")
-    dsm.edgelist <- as.data.frame(get.edgelist(dsm.graph))
-
-    return(dsm.edgelist)
-}
 
 load.sdsm <- function(sdsm.filename, relavent.files) {
     sdsm.size <- read.table(sdsm.filename, skip=1, nrows=1)[[1]]
