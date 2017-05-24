16d5969fe913bed4383b4a8241041f5c1c9a5bb2
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2017-05-24 12:15:26 -0300

    Add copyright header to sourceAnalysis.py
    GPLv2 as usual.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
    CC: Mitchell Joblin <mitchell.joblin@siemens.com>

--- codeface/sourceAnalysis.py
+++ codeface/sourceAnalysis.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python
 
+# This file is part of Codeface. Codeface is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Copyright 2014, Siemens AG, Mitchell Joblin <mitchell.joblin.ext@siemens.com>
+# Copyright 2017, Wolfgang Mauerer <wm@linux-kernel.net>
+# All Rights Reserved.
+
 import xml.etree.ElementTree as ET
 import os
 import sys
