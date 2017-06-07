ac772eb111af08c72c4fdb176095fe41e81a6665
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2017-06-07 16:05:08 +0200

    Add time series plots for absolute correlation date to global conway stage
    Add two new plots that display how the quantities in the correlation
    plot develop over time in terms of absolute numbers.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

--- codeface/R/conway_global.r
+++ codeface/R/conway_global.r
@@ -19,6 +19,7 @@
 s <- suppressPackageStartupMessages
 s(library(ggplot2))
 s(library(lubridate))
+s(library(dplyr))
 
 source("query.r")
 source("utils.r")
@@ -26,21 +27,32 @@ source("config.r")
 source("dependency_analysis.r")
 source("quality_analysis.r")
 
-get.correlation.data <- function(conf, i, cycles, quality.file, quality.type) {
+get.correlation.data <- function(cycles, i, quality.file, quality.type, do.subset=TRUE) {
     if (!file.exists(quality.file)) {
         return(NULL)
     }
     artifacts.dat <- read.csv(quality.file)
-    corr.elements <- gen.correlation.columns(quality.type)
-    cycle <- cycles[i, ]
 
-    artifacts.subset <- artifacts.dat[, corr.elements$names]
-    colnames(artifacts.subset) <- corr.elements$labels
+    if (do.subset) {
+        corr.elements <- gen.correlation.columns(quality.type)
+        artifacts.dat <- artifacts.dat[, corr.elements$names]
+        colnames(artifacts.dat) <- corr.elements$labels
+    }
 
-    if (nrow(artifacts.subset) == 0) {
+    if (nrow(artifacts.dat) == 0) {
         return(NULL)
     }
 
+    return(artifacts.dat)
+}
+
+compute.correlation.data <- function(conf, i, cycles, quality.file, quality.type) {
+    artifacts.subset <- get.correlation.data(cycles, i, quality.file, quality.type)
+    if (is.null(artifacts.subset)) {
+        return(NULL)
+    }
+
+    cycle <- cycles[i, ]
     corr.mat <- cor(artifacts.subset, use="pairwise.complete.obs",
                     method="spearman")
     corr.test <- cor.mtest(artifacts.subset) ## Note: can be NULL
@@ -56,9 +68,18 @@ get.correlation.data <- function(conf, i, cycles, quality.file, quality.type) {
                       value=corr.mat[upper.tri(corr.combinations)]))
 }
 
+make.title <- function(conf, motif.type) {
+    return(str_c(conf$description, " (window: ", conf$windowSize, " months, motif: ",
+                 motif.type, ", comm: ", conf$communicationType, ")"))
+}
+
 dispatch.all <- function(conf, resdir) {
     motif.type <- list("triangle", "square")[[1]]
     cycles <- get.cycles(conf)
+
+    if (is.null(conf$windowSize)) {
+        conf$windowSize <- 3
+    }
 
     ## Compute correlation values time series and plot the result
     res <- lapply(1:nrow(cycles), function(i) {
@@ -67,7 +88,7 @@ dispatch.all <- function(conf, resdir) {
                                   conf$communicationType, "quality_data.csv")
 
         logdevinfo(str_c("Analysing quality file ", quality.file), logger="conway")
-        return(get.correlation.data(conf, i, cycles, quality.file, conf$qualityType))
+        res <- compute.correlation.data(conf, i, cycles, quality.file, conf$qualityType)
     })
 
     corr.dat <- do.call(rbind, res)
@@ -79,11 +100,53 @@ dispatch.all <- function(conf, resdir) {
     corr.label <- "Correlated\nQuantities"
     g <- ggplot(corr.dat, aes(x=date, y=value, colour=combination, shape=combination)) +
         geom_point() + geom_line() +  scale_x_date("Date", date_labels="%m-%Y") + ylab("Correlation") +
-        ggtitle(conf$description) + scale_colour_discrete(corr.label) +
+        ggtitle(make.title(conf, motif.type)) + scale_colour_discrete(corr.label) +
         scale_shape_discrete(corr.label) + theme_bw()
     ggsave(plot.file, g, width=7, height=3)
 
 
+    ## ###############################################################
+    ## Compute a time series with absolute data counts for the previous correlation computations
+    res <- lapply(1:nrow(cycles), function(i) {
+        range.resdir <- file.path(resdir, gen.range.path(i, cycles[i,]$cycle))
+        quality.file <- file.path(range.resdir, "quality_analysis", motif.type,
+                                  conf$communicationType, "quality_data.csv")
+
+        logdevinfo(str_c("Analysing quality file ", quality.file), logger="conway")
+        res <- get.correlation.data(cycles, i, quality.file, conf$qualityType, do.subset=FALSE)
+        if (is.null(res)) {
+            return(NULL)
+        }
+
+        res$date <- cycles[i,]$date.end
+        res$range <- i
+        return(res)
+    })
+    res <- do.call(rbind, res)
+
+    plot.file <- file.path(resdir, str_c("abs_ts_", motif.type, "_",
+                                        conf$communicationType, ".pdf"))
+
+    labels <- c(motif.count = "Motifs", motif.anti.count = "Anti-Motifs")
+    corr.dat$date <- as.Date(corr.dat$date)
+    if (conf$communicationType=="mail") {
+        res <- res[,c("corrective", "motif.count", "motif.anti.count", "dev.count", "date", "range")]
+        res$date <- as.Date(res$date)
+        res.molten <- melt(res, measure.vars=c("motif.count", "motif.anti.count"))
+
+        g <- ggplot(res.molten, aes(x=dev.count, y=value)) + geom_point(size=0.5) +
+            facet_grid(variable~date, labeller=labeller(variable=labels)) +
+            scale_x_sqrt("# Devs contributing to artifact [sqrt]") +
+            scale_y_sqrt("Count [sqrt]") + geom_smooth(method=lm) + theme_bw() +
+            ggtitle(make.title(conf, motif.type))
+        logdevinfo(str_c("Saving plot to ", plot.file), logger="conway")
+        ggsave(plot.file, g, width=8, height=5)
+    }
+    else if (conf$communicationType=="jira") {
+        ## TODO: Provide the plot for jira
+    }
+
+    ## ###########################################################
     ## Prepare a global "timeseries" plot of the null model tests
     res <- do.call(rbind, lapply(1:nrow(cycles), function(i) {
         range.resdir <- file.path(resdir, gen.range.path(i, cycles[i,]$cycle))
@@ -103,21 +166,29 @@ dispatch.all <- function(conf, resdir) {
 
     res$date <- as.Date(res$date)
     labels <- c(negative = "Anti-Motif", positive = "Motif", ratio = "Ratio")
-    if (is.null(conf$windowSize)) {
-        window.size <- 3
-    } else {
-        window.size <- conf$windowSize
-    }
     g <- ggplot(data=res, aes(x=count)) +
         geom_point(aes(x=empirical.count), y=0, color="red", size=2.5) +
         geom_density(aes(x=count, y=..scaled..), alpha=.2, fill="#AAD4FF") +
         facet_wrap(count.type~date, nrow=3, scales="free_x", labeller=labeller(count.type=labels)) +
         xlab("Count or Ratio") + ylab("Density [a.u.]") +
-        ggtitle(str_c(conf$description, " (analysis window: ", window.size, " months)")) + theme_bw()
+        ggtitle(make.title(conf, motif.type)) + theme_bw()
 
     plot.file <- file.path(resdir, str_c("motif_ts_", motif.type, "_",
                                          conf$communicationType, ".pdf"))
+    logdevinfo(str_c("Saving plot to ", plot.file), logger="conway")
     ggsave(plot.file, g, width=9, height=6)
+
+    ## #####################################################
+    ## Plot a time series with absolute empirical motif counts
+    plot.file <- file.path(resdir, str_c("motif_ts_abs_", motif.type, "_",
+                                         conf$communicationType, ".pdf"))
+    g <- ggplot(res, aes(x=as.Date(date), y=empirical.count)) + geom_point() + geom_line() +
+        facet_grid(count.type~., scales="free_y", labeller=labeller(count.type=labels)) +
+        scale_x_date("Date", date_labels="%m-%Y") +
+        ylab("Count or Ratio") + theme_bw() + ggtitle(make.title(conf, motif.type))
+
+    logdevinfo(str_c("Saving plot to ", plot.file), logger="conway")
+    ggsave(plot.file, g, width=7, height=4)
 }
 
 config.script.run({
