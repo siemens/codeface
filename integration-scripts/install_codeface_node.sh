1b3781ebe42a4079811d07894c8bfe1abcbc0860
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2018-10-26 21:15:20 +0200

    Ensure that node modules can be installed
    The target folder for node modules as required by the
    ID service does not necesarily belong to the user running
    vagrant. Make sure to set ownership appropriately.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

--- integration-scripts/install_codeface_node.sh
+++ integration-scripts/install_codeface_node.sh
@@ -6,6 +6,8 @@ echo "Providing id_service"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install npm
 cd id_service
+sudo mkdir node_modules
+sudo chown vagrant.vagrant node_modules
 npm set ca null
 npm install --no-bin-links
 cd ..
